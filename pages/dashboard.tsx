6a9ff6bd58178929671ef7cb13f56e65ec907001
John Wineman <john.wineman@gmail.com>
2020-09-09 11:00:13 -0700

    feat: Add Dashboard desktop layout.

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,9 @@
+import styled from 'styled-components';
+
 import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
+
+import { FlexDiv, CapitalizedText } from 'styles/common';
 
 const DashboardPage = () => {
 	const { t } = useTranslation();
@@ -9,9 +13,39 @@ const DashboardPage = () => {
 			<Head>
 				<title>{t('dashboard.page-title')}</title>
 			</Head>
-			<div>Dashboard</div>
+			<CardsContainer>
+				<LeftCardContainer>
+					<CapitalizedText>
+						<div>{t('dashboard.your-profile.title')}</div>
+					</CapitalizedText>
+				</LeftCardContainer>
+				<RightCardContainer>
+					<CapitalizedText>{t('dashboard.watchlist.title')}</CapitalizedText>
+				</RightCardContainer>
+			</CardsContainer>
 		</>
 	);
 };
+
+const CardsContainer = styled(FlexDiv)`
+	justify-content: space-between;
+	width: 100%;
+`;
+
+const LeftCardContainer = styled.div`
+	flex: 1;
+	max-width: 1000px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 48px 0px;
+`;
+
+const RightCardContainer = styled.div`
+	width: 356px;
+	background-color: ${(props) => props.theme.colors.elderberry};
+	padding: 48px 32px;
+`;
 
 export default DashboardPage;
