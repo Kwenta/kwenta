23c116f29d1bc311c07526a757d4317baa3a98fd
John Wineman <john.wineman@gmail.com>
2020-09-10 10:30:40 -0700

    feat: <NoSynths/> dashboard view.

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import synthetix from 'lib/synthetix';
+import { useRecoilValue } from 'recoil';
 
 import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
+import { priceCurrencyState } from 'store/app';
 
-import { FlexDiv, CapitalizedText } from 'styles/common';
+import { FlexDiv, CapitalizedText, FlexDivRow, FlexDivCol } from 'styles/common';
 import { TabList, TabPanel, TabButton } from 'components/Tab';
+import useSynthsBalancesQuery, {
+	SynthBalance,
+} from 'queries/walletBalances/useSynthsBalancesQuery';
+import Currency from 'components/Currency';
+import useExchangeRatesQuery from 'queries/rates/useExchangeRatesQuery';
+import { fonts } from 'styles/theme/fonts';
+import Button from 'components/Button';
 
 const TABS = {
 	SYNTH_BALANCES: 'synth-balances',
@@ -14,7 +24,45 @@ const TABS = {
 	TRANSACTIONS: 'transactions',
 };
 
-const SynthBalances = () => <>Synth Balances</>;
+const SynthBalances = () => {
+	const exchangeRatesQuery = useExchangeRatesQuery({ refetchInterval: false });
+	const synthsBalancesQuery = useSynthsBalancesQuery({ enabled: exchangeRatesQuery.isSuccess });
+	const selectedPriceCurrency = useRecoilValue(priceCurrencyState);
+	return (
+		<FlexDiv>
+			{synthsBalancesQuery.isSuccess &&
+				synthsBalancesQuery.data.balances.map((synth: SynthBalance) => {
+					const synthDesc =
+						synthetix.synthsMap != null ? synthetix.synthsMap[synth.currencyKey]?.desc : '';
+					return (
+						<FlexDivRow>
+							<FlexDivCol>
+								<Currency.Name currencyKey={synth.currencyKey} name={synthDesc} showIcon={true} />
+							</FlexDivCol>
+							<FlexDivCol>
+								<Currency.Amount
+									currencyKey={synth.currencyKey}
+									amount={synth.balance}
+									totalValue={synth.usdBalance}
+									sign={selectedPriceCurrency.sign}
+								/>
+							</FlexDivCol>
+							<FlexDivCol>
+								{exchangeRatesQuery.data !== undefined && (
+									<Currency.Price
+										currencyKey={synth.currencyKey}
+										price={exchangeRatesQuery.data[synth.currencyKey]}
+										sign={selectedPriceCurrency.sign}
+									/>
+								)}
+							</FlexDivCol>
+							<FlexDivCol>Percent</FlexDivCol>
+						</FlexDivRow>
+					);
+				})}
+		</FlexDiv>
+	);
+};
 
 const DashboardPage = () => {
 	const { t } = useTranslation();
@@ -27,6 +75,7 @@ const DashboardPage = () => {
 			<CardsContainer>
 				<LeftCardContainer>
 					<CapitalizedText>
+						<NoSynths />
 						<div>{t('dashboard.your-profile.title')}</div>
 						<TabList>
 							<TabButton
@@ -65,12 +114,17 @@ const DashboardPage = () => {
 					</CapitalizedText>
 				</LeftCardContainer>
 				<RightCardContainer>
-					<CapitalizedText>{t('dashboard.watchlist.title')}</CapitalizedText>
+					<RightBarTitle>{t('dashboard.watchlist.title')}</RightBarTitle>
 				</RightCardContainer>
 			</CardsContainer>
 		</>
 	);
 };
+
+const RightBarTitle = styled.div`
+	${fonts.body['bold-medium']}
+	color: ${(props) => props.theme.colors.white};
+`;
 
 const CardsContainer = styled(FlexDiv)`
 	justify-content: space-between;
@@ -92,5 +146,39 @@ const RightCardContainer = styled.div`
 	background-color: ${(props) => props.theme.colors.elderberry};
 	padding: 48px 32px;
 `;
+
+const NoSynthTitle = styled.div`
+	${fonts.data['title-small']}
+	color: ${(props) => props.theme.colors.blueberry};
+	text-transform: uppercase;
+	text-align: center;
+	margin-bottom: 4px;
+`;
+
+const NoSynthSubtitle = styled.div`
+	${fonts.heading.h4}
+	color: ${(props) => props.theme.colors.white};
+	text-align:center;
+	margin-bottom: 33px;
+`;
+
+const Center = styled.div`
+	margin: 0 auto;
+	margin-bottom: 78px;
+`;
+
+const NoSynths = () => (
+	<FlexDiv>
+		<FlexDivCol>
+			<NoSynthTitle>Learn How it Works</NoSynthTitle>
+			<NoSynthSubtitle>Get up and running on Kwenta and exchange everything.</NoSynthSubtitle>
+			<Center>
+				<Button variant="primary" isRounded={true} size="lg">
+					Learn More
+				</Button>
+			</Center>
+		</FlexDivCol>
+	</FlexDiv>
+);
 
 export default DashboardPage;
--- styles/theme/fonts.ts
+++ styles/theme/fonts.ts
@@ -12,6 +12,27 @@ export const fonts = {
 			line-height: 140%;
 			letter-spacing: 0px;
 		`,
+		'bold-medium': `
+			font-family: ${fontStyles.bold};
+			font-size: 14px;
+			line-height: 140%;
+			letter-spacing: 0.2px;
+		`,
+	},
+	data: {
+		'title-small': `
+			font-family: ${fontStyles.mono};
+			font-size: 12px;
+			line-height: 140%;
+			letter-spacing: 0.2px;
+		`,
+	},
+	heading: {
+		h4: `
+			font-family: ${fontStyles.regular};
+			font-size: 20px;
+			line-height: 140%;
+		`,
 	},
 };
 
