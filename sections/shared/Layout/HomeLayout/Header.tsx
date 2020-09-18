7f0d12ec921e4af2df84c2c522a08bd6067d78f3
AndyTCF <andy.c.f.98@gmail.com>
2020-09-18 16:28:46 +1000

    Finish home header

--- sections/shared/Layout/HomeLayout/Header.tsx
+++ sections/shared/Layout/HomeLayout/Header.tsx
@@ -1,30 +1,70 @@
 import { FC } from 'react';
 import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+
+import { ExternalLink, FlexDivCentered, TextButton } from 'styles/common';
+import { HEADER_HEIGHT } from 'constants/ui';
+import Button from 'components/Button';
 
 import Logo from '../Logo';
 
-import { GridDivCenteredCol } from 'styles/common';
-import { HEADER_HEIGHT } from 'constants/ui';
-
-const Header: FC = () => (
-	<Container>
-		<LogoNav>
-			<Logo />
-		</LogoNav>
-	</Container>
-);
+const Header: FC = () => {
+	const { t } = useTranslation();
+	return (
+		<Container>
+			<Section left>
+				<Logo />
+			</Section>
+			<Section>
+				<ExternalLink>
+					<Link>{t('homepage.nav.why')}</Link>
+				</ExternalLink>
+				<ExternalLink>
+					<Link>{t('homepage.nav.how')}</Link>
+				</ExternalLink>
+				<ExternalLink>
+					<Link>{t('homepage.nav.faq')}</Link>
+				</ExternalLink>
+			</Section>
+			<Section right>
+				<WalletButton>{t('homepage.nav.wallet')}</WalletButton>
+				<Button variant="primary" isRounded={false} size="md">
+					{t('homepage.nav.exchange')}
+				</Button>
+			</Section>
+		</Container>
+	);
+};
 
 const Container = styled.header`
 	height: ${HEADER_HEIGHT};
 	line-height: ${HEADER_HEIGHT};
-	padding: 0 20px;
+	padding: 0px 20px;
+	margin-top: 10px;
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
+	width: 100%;
 `;
 
-const LogoNav = styled(GridDivCenteredCol)`
-	grid-gap: 24px;
+const Section = styled<any>(FlexDivCentered)`
+	width: 33%;
+	justify-content: ${(props) => (props.left ? 'flex-start' : props.right ? 'flex-end' : 'center')};
+`;
+
+const Link = styled(TextButton)`
+	font-size: 12px;
+	font-weight: bold;
+	text-align: center;
+	color: #8a939f;
+	margin: 0px 8px;
+`;
+
+const WalletButton = styled(TextButton)`
+	color: #ffffff;
+	font-size: 12px;
+	font-weight: 700;
+	margin-right: 16px;
 `;
 
 export default Header;
