4846bc0e6de431d9fdb22b3a74bb70abddd8746e
AndyTCF <andy.c.f.98@gmail.com>
2020-09-21 16:11:07 +1000

    Reset back to default

--- sections/exchange/FooterCard/NoSynthsCard/NoSynthsCard.tsx
+++ sections/exchange/FooterCard/NoSynthsCard/NoSynthsCard.tsx
@@ -7,16 +7,12 @@ import { CRYPTO_CURRENCY_MAP, SYNTHS_MAP } from 'constants/currency';
 import { NoTextTransform, ExternalLink } from 'styles/common';
 
 import { RoundedContainer, Message, MessageButton } from '../common';
-import OneInch from 'containers/OneInch';
 
 const { sUSD } = SYNTHS_MAP;
 const { ETH } = CRYPTO_CURRENCY_MAP;
 
 const NoSynthsCard: FC = () => {
 	const { t } = useTranslation();
-
-	const { swap } = OneInch.useContainer();
-
 	return (
 		<RoundedContainer>
 			<Message>
@@ -27,16 +23,16 @@ const NoSynthsCard: FC = () => {
 					components={[<NoTextTransform />]}
 				/>
 			</Message>
-			{/* <ExternalLink href={EXTERNAL_LINKS.Trading.OneInchLink(ETH, sUSD)}> */}
-			<MessageButton onClick={() => swap('0.5')}>
-				<Trans
-					t={t}
-					i18nKey="common.currency.get-currency"
-					values={{ currencyKey: sUSD }}
-					components={[<NoTextTransform />]}
-				/>
-			</MessageButton>
-			{/* </ExternalLink> */}
+			<ExternalLink href={EXTERNAL_LINKS.Trading.OneInchLink(ETH, sUSD)}>
+				<MessageButton>
+					<Trans
+						t={t}
+						i18nKey="common.currency.get-currency"
+						values={{ currencyKey: sUSD }}
+						components={[<NoTextTransform />]}
+					/>
+				</MessageButton>
+			</ExternalLink>
 		</RoundedContainer>
 	);
 };
