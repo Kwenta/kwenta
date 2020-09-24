f7dce6cc1075756a5aecefce5ee9956e11cfe258
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-09-24 10:46:03 +1000

    feat: add meta tags / copy update on page titles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
+import { useTranslation } from 'react-i18next';
 
 import { ThemeProvider } from 'styled-components';
 import { MediaContextProvider } from 'styles/media';
@@ -19,6 +20,8 @@ import '../i18n';
 import Layout from 'sections/shared/Layout';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
+	const { t } = useTranslation();
+
 	return (
 		<>
 			<Head>
@@ -27,21 +30,21 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
 					name="viewport"
 					content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
 				/>
-				<meta name="description" content="Kwenta description" />
+				<meta name="description" content={t('meta.description')} />
 				<meta name="twitter:card" content="summary_large_image" />
 				<meta name="twitter:site" content="@kwenta_io" />
 				<meta name="twitter:creator" content="@kwenta_io" />
 				{/* open graph */}
 				<meta property="og:url" content="https://kwenta.io/" />
 				<meta property="og:type" content="website" />
-				<meta property="og:title" content="Kwenta" />
-				<meta property="og:description" content="Kwenta description" />
+				<meta property="og:title" content={t('meta.og.title')} />
+				<meta property="og:description" content={t('meta.description')} />
 				<meta property="og:image" content="/images/kwenta.png" />
-				<meta property="og:image:alt" content="Kwenta" />
-				<meta property="og:site_name" content="Kwenta" />
+				<meta property="og:image:alt" content={t('meta.og.title')} />
+				<meta property="og:site_name" content={t('meta.og.site-name')} />
 				{/* twitter */}
-				<meta name="twitter:image" content="/images/kwenta.png" />
-				<meta name="twitter:url" content="https://kwenta.io" />
+				{/* <meta name="twitter:image" content="/images/kwenta.png" />
+				<meta name="twitter:url" content="https://kwenta.io" /> */}
 				<link rel="icon" href="/images/favicon.svg" />
 			</Head>
 			<ThemeProvider theme={theme}>
