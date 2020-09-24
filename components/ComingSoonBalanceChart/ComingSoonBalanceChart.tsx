dca0fa194bed5573aa590ca6b17c7e5c6834e9b2
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-09-24 12:52:29 +1000

    refactor: style the coming soon chart a bit

--- components/ComingSoonBalanceChart/ComingSoonBalanceChart.tsx
+++ components/ComingSoonBalanceChart/ComingSoonBalanceChart.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useContext } from 'react';
+import styled, { ThemeContext } from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import { AreaChart, Area, XAxis, YAxis } from 'recharts';
 
 import RechartsResponsiveContainer from 'components/RechartsResponsiveContainer';
-
-import Card from 'components/Card';
 
 // TODO: implement using real data
 import { MOCK_DATA } from './mockData';
@@ -13,36 +11,37 @@ import { fonts } from 'styles/theme/fonts';
 
 export const ComingSoonBalanceChart = () => {
 	const { t } = useTranslation();
+	const theme = useContext(ThemeContext);
+
+	const chartColor = theme.colors.green;
 
 	return (
-		<Card>
-			<StyledCardBody>
-				<StyledResponsiveContainer width="99%" height="100%">
-					<AreaChart data={MOCK_DATA}>
-						<defs>
-							<linearGradient id="synthBalanceChartArea" x1="0" y1="0" x2="0" y2="1">
-								<stop offset="5%" stopColor="#FFF" stopOpacity={0.5} />
-								<stop offset="95%" stopColor="#FFF" stopOpacity={0} />
-							</linearGradient>
-						</defs>
-						<XAxis dataKey="timestamp" hide={true} />
-						<YAxis type="number" domain={['auto', 'auto']} hide={true} />
-						<Area
-							dataKey="rate"
-							stroke="#FFF"
-							fillOpacity={0.5}
-							fill="url(#synthBalanceChartArea)"
-							isAnimationActive={false}
-						/>
-					</AreaChart>
-				</StyledResponsiveContainer>
-				<Message>{t('common.features.coming-soon')}</Message>
-			</StyledCardBody>
-		</Card>
+		<Container>
+			<StyledResponsiveContainer>
+				<AreaChart data={MOCK_DATA} margin={{ right: 0, bottom: 0, left: 0, top: 0 }}>
+					<defs>
+						<linearGradient id="synthBalanceChartArea" x1="0" y1="0" x2="0" y2="1">
+							<stop offset="5%" stopColor={chartColor} stopOpacity={0.5} />
+							<stop offset="95%" stopColor={chartColor} stopOpacity={0} />
+						</linearGradient>
+					</defs>
+					<XAxis dataKey="timestamp" hide={true} />
+					<YAxis type="number" domain={['auto', 'auto']} hide={true} />
+					<Area
+						dataKey="rate"
+						stroke={chartColor}
+						fillOpacity={0.5}
+						fill="url(#synthBalanceChartArea)"
+						isAnimationActive={false}
+					/>
+				</AreaChart>
+			</StyledResponsiveContainer>
+			<Message>{t('common.features.coming-soon')}</Message>
+		</Container>
 	);
 };
 
-const StyledCardBody = styled(Card.Body)`
+const Container = styled.div`
 	padding: 0;
 	height: 120px;
 	position: relative;
