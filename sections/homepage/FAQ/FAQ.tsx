c964a5169282d4fbb6a9f2035ae98060062c5293
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-09-29 16:35:24 +1000

    refactor(FAQ): use a more "complex" approach for the translations to support links

--- sections/homepage/FAQ/FAQ.tsx
+++ sections/homepage/FAQ/FAQ.tsx
@@ -1,28 +1,75 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useMemo } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs';
 import { Accordion, AccordionItem, AccordionButton, AccordionPanel } from '@reach/accordion';
+
+import { EXTERNAL_LINKS } from 'constants/links';
 
 import PlusThinIcon from 'assets/inline-svg/app/plus-thin.svg';
 
 import media from 'styles/media';
 
 import { StackSection, CenterSubHeader } from '../common';
-import { resetButtonCSS } from 'styles/common';
+import { ExternalLink, resetButtonCSS } from 'styles/common';
 
 type Item = {
 	question: string;
 	answer: string;
+	link?: string;
 };
 
 const FAQ = () => {
 	const { t } = useTranslation();
 
-	const tabs = t('homepage.faq.tabs', {
-		returnObjects: true,
-	}) as Array<{ title: string; items: Item[] }>;
+	const tabs = useMemo(
+		() => [
+			{
+				title: t('homepage.faq.tabs.getting-started.title'),
+				items: [
+					{
+						question: t('homepage.faq.tabs.getting-started.items.qna1.question'),
+						answer: t('homepage.faq.tabs.getting-started.items.qna1.answer'),
+						link: 'https://www.youtube.com/watch?v=TDGq4aeevgY',
+					},
+					{
+						question: t('homepage.faq.tabs.getting-started.items.qna2.question'),
+						answer: t('homepage.faq.tabs.getting-started.items.qna2.answer'),
+						link: 'https://www.youtube.com/watch?v=k9HYC0EJU6E',
+					},
+					{
+						question: t('homepage.faq.tabs.getting-started.items.qna3.question'),
+						answer: t('homepage.faq.tabs.getting-started.items.qna3.answer'),
+					},
+				],
+			},
+			{
+				title: t('homepage.faq.tabs.trading-on-kwenta.title'),
+				items: [
+					{
+						question: t('homepage.faq.tabs.trading-on-kwenta.items.qna1.question'),
+						answer: t('homepage.faq.tabs.trading-on-kwenta.items.qna1.answer'),
+					},
+					{
+						question: t('homepage.faq.tabs.trading-on-kwenta.items.qna2.question'),
+						answer: (
+							<Trans
+								t={t}
+								i18nKey="homepage.faq.tabs.trading-on-kwenta.items.qna2.answer"
+								components={[<ExternalLink href={EXTERNAL_LINKS.Synthetix.Litepaper} />]}
+							/>
+						),
+					},
+					{
+						question: t('homepage.faq.tabs.trading-on-kwenta.items.qna3.question'),
+						answer: t('homepage.faq.tabs.trading-on-kwenta.items.qna3.answer'),
+					},
+				],
+			},
+		],
+		[t]
+	) as Array<{ title: string; items: Item[] }>;
 
 	return (
 		<StyledStackSection id="faq">
@@ -37,14 +84,23 @@ const FAQ = () => {
 					{tabs.map(({ title, items }) => (
 						<TabPanel key={title}>
 							<StyledAccordion collapsible={true} multiple={true}>
-								{items.map(({ question, answer }) => (
+								{items.map(({ question, answer, link }) => (
 									<AccordionItem key={question}>
 										<AccordionButton>
 											{question}
 											<AccordionOpenIcon />
 											<AccordionCloseIcon />
 										</AccordionButton>
-										<AccordionPanel>{answer}</AccordionPanel>
+										<AccordionPanel>
+											{answer}
+											{link && (
+												<div>
+													<ExternalLink href={link}>
+														{t('homepage.faq.tabs.explainer-video')}
+													</ExternalLink>
+												</div>
+											)}
+										</AccordionPanel>
 									</AccordionItem>
 								))}
 							</StyledAccordion>
@@ -111,6 +167,12 @@ const StyledTabs = styled(Tabs)`
 	}
 	[data-reach-tab-panel] {
 		outline: none;
+		a {
+			color: ${(props) => props.theme.colors.purple};
+			&:hover {
+				color: ${(props) => props.theme.colors.purpleHover};
+			}
+		}
 	}
 `;
 
