45da165a4681b3a013dadfd5897a6b1d784e573e
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-10-01 21:02:43 +1000

    a11y: enable zoom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,10 +28,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
 		<>
 			<Head>
 				<meta charSet="utf-8" />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
-				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta name="description" content={t('meta.description')} />
 				{/* open graph */}
 				<meta property="og:url" content="https://kwenta.io/" />
