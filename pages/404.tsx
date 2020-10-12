3bfc09e9306ca60ecca3ccc610d8e812f568007c
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-10-12 14:04:27 +1100

    feat: impl. 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,14 @@
 import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+import AppLayout from 'sections/shared/Layout/AppLayout';
+
+import media from 'styles/media';
+import { FlexDivRowCentered } from 'styles/common';
+
+import CaretLeftXLIcon from 'assets/inline-svg/app/caret-left-xl.svg';
+import CaretRightXLICon from 'assets/inline-svg/app/caret-right-xl.svg';
 
 const NotFoundPage = () => {
 	const { t } = useTranslation();
@@ -9,9 +18,63 @@ const NotFoundPage = () => {
 			<Head>
 				<title>{t('not-found.page-title')}</title>
 			</Head>
-			<div>Not Found</div>
+			<AppLayout>
+				<Container>
+					<CaretLeftXLIcon />
+					<Content>
+						<Title>{t('not-found.title')}</Title>
+						<Subtitle>{t('not-found.subtitle')}</Subtitle>
+					</Content>
+					<CaretRightXLICon />
+				</Container>
+			</AppLayout>
 		</>
 	);
 };
+
+const Container = styled(FlexDivRowCentered)`
+	flex-grow: 1;
+	justify-content: center;
+	display: flex;
+	align-items: center;
+	> * {
+		flex-shrink: 0;
+	}
+	padding: 0 20px;
+	${media.lessThan('sm')`
+		flex-direction: column;
+		svg {
+			transform: rotate(90deg);
+		}
+	`}
+`;
+
+const Content = styled.div`
+	color: ${(props) => props.theme.colors.white};
+	font-family: ${(props) => props.theme.fonts.mono};
+	padding: 0 48px;
+	text-align: center;
+	margin-top: -10px;
+`;
+
+const Title = styled.h1`
+	font-size: 120px;
+	margin: 0;
+	font-weight: normal;
+	line-height: normal;
+	${media.lessThan('sm')`
+		font-size: 80px;
+	`}
+`;
+
+const Subtitle = styled.h2`
+	margin: 0;
+	font-size: 20px;
+	line-height: normal;
+	font-weight: normal;
+	${media.lessThan('sm')`
+		font-size: 14px;
+	`}
+`;
 
 export default NotFoundPage;
