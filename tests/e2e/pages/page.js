bc908be77894871ae1910d0778304ffb90904368
Jakub Mucha <jakub.mucha@icloud.com>
2020-12-02 13:58:06 +0100

    e2e(po): add base page
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

---
+++ tests/e2e/pages/page.js
@@ -0,0 +1,13 @@
+export default class Page {
+	getTitle() {
+		return cy.title();
+	}
+
+	getMetamaskWalletAddress() {
+		return cy.fetchMetamaskWalletAddress();
+	}
+
+	acceptMetamaskAccessRequest() {
+		cy.acceptMetamaskAccess();
+	}
+}
