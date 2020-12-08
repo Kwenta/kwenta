2b9abac9032f42b2f5731b1e22be417e63f6d570
Jakub Mucha <jakub.mucha@icloud.com>
2020-12-09 00:25:27 +0100

    e2e: add basic trading test for sUSD => sETH
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

---
+++ tests/e2e/specs/trade-spec.js
@@ -0,0 +1,27 @@
+import ExchangePage from '../pages/exchange/exchange-page';
+
+const exchange = new ExchangePage();
+
+describe('Trades tests', () => {
+	context('Trade sUSD => sETH', () => {
+		before(() => {
+			exchange.visit('sETH-sUSD');
+			exchange.connectMetamaskWallet();
+			exchange.waitUntilLoggedIn();
+		});
+		it(`should exchange with success`, () => {
+			exchange.getCurrencyAmount().type('1');
+			exchange.getSubmitOrderBtn().click();
+			exchange.confirmMetamaskTransaction();
+			exchange.waitForTransactionSuccess();
+			exchange.getTransactionUrl().then((url) => {
+				exchange.waitUntilAvailableOnEtherscan(url, 'etherscan');
+				cy.get('@etherscan').should((response) => {
+					expect(response.body).to.include('</i>Success</span>');
+					// blocker: need slippage explanations
+					// todo: verify gas amount in metamask and etherscan, etherscan asset sent, fee and received asset value
+				});
+			});
+		});
+	});
+});
