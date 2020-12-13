bed000b8e543fdda07bb62bc517197470a6526e8
Jakub Mucha <jakub.mucha@icloud.com>
2020-12-13 13:54:20 +0100

    po: add snx-related commands
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- tests/e2e/pages/page.js
+++ tests/e2e/pages/page.js
@@ -18,16 +18,38 @@ export default class Page {
 		cy.confirmMetamaskTransaction();
 	}
 
-	waitUntilAvailableOnEtherscan(url, alias) {
-		cy.request(url).as(alias);
-		cy.get(`@${alias}`).then((response) => {
-			if (
-				response.body.includes('This transaction has been included into Block No') ||
-				response.body.includes('</i> Pending</span>')
-			) {
-				cy.wait(10000);
-				this.waitUntilAvailableOnEtherscan(url, alias);
-			}
-		});
+	snxExchangerSettle(asset) {
+		return cy.snxExchangerSettle(asset);
 	}
+
+	snxCheckWaitingPeriod(asset) {
+		cy.snxCheckWaitingPeriod(asset);
+	}
+
+	waitUntilAvailableOnEtherscan(urlOrTx, alias) {
+		if (!urlOrTx.includes('http')) {
+			cy.getNetwork().then((network) => {
+				const etherscanUrl =
+					network.networkName === 'mainnet'
+						? `https://etherscan.io/tx/${urlOrTx}`
+						: `https://${network.networkName}.etherscan.io/tx/${urlOrTx}`;
+				waitForTxSuccess(etherscanUrl, alias);
+			});
+		} else {
+			waitForTxSuccess(urlOrTx, alias);
+		}
+	}
+}
+
+function waitForTxSuccess(url, alias) {
+	cy.request(url).as(alias);
+	cy.get(`@${alias}`).then((response) => {
+		if (
+			response.body.includes('This transaction has been included into Block No') ||
+			response.body.includes('</i> Pending</span>')
+		) {
+			cy.wait(5000);
+			waitForTxSuccess(url, alias);
+		}
+	});
 }
