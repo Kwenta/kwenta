876495adaefcb05d4a4e12a5e2a0d2fadeb47634
Mitch <60554509+vbstreetz@users.noreply.github.com>
2021-04-05 14:47:18 +0300

    fix: lookup current shorting snx rewards rate & duration from contract (#318)

--- queries/collateral/useCollateralShortStats.ts
+++ queries/collateral/useCollateralShortStats.ts
@@ -2,7 +2,6 @@ import { useQuery, QueryConfig } from 'react-query';
 import { useRecoilValue } from 'recoil';
 import { ethers } from 'ethers';
 import BigNumber from 'bignumber.js';
-import zipObject from 'lodash/zipObject';
 
 import { CurrencyKey } from 'constants/currency';
 import { appReadyState } from 'store/app';
@@ -12,27 +11,50 @@ import QUERY_KEYS from 'constants/queryKeys';
 import synthetix from 'lib/synthetix';
 import { toBigNumber } from 'utils/formatters/number';
 
+type ReturnValueType = Record<
+	CurrencyKey,
+	{
+		shorts: BigNumber;
+		rewardsRate: BigNumber;
+		rewardsTotalSupply: BigNumber;
+	}
+>;
+
 const useCollateralShortStats = (
 	currencyKeys: CurrencyKey[],
-	options?: QueryConfig<Record<CurrencyKey, BigNumber>>
+	options?: QueryConfig<ReturnValueType>
 ) => {
 	const isAppReady = useRecoilValue(appReadyState);
 
-	return useQuery<Record<CurrencyKey, BigNumber>>(
+	return useQuery<ReturnValueType>(
 		QUERY_KEYS.Collateral.ShortStats(currencyKeys.join('|')),
 		async () => {
-			const shorts = (await Promise.all(
-				currencyKeys.map((currencyKey) =>
+			const stats = (await Promise.all([
+				...currencyKeys.map((currencyKey) =>
 					synthetix.js!.contracts.CollateralManager.short(
 						ethers.utils.formatBytes32String(currencyKey)
 					)
-				)
-			)) as ethers.BigNumber[];
+				),
+				...currencyKeys.map((currencyKey) =>
+					synthetix.js!.contracts[`ShortingRewards${currencyKey}`].rewardRate()
+				),
+				...currencyKeys.map((currencyKey) =>
+					synthetix.js!.contracts[`ShortingRewards${currencyKey}`].totalSupply()
+				),
+			])) as ethers.BigNumber[];
 
-			return zipObject(
-				currencyKeys,
-				shorts.map((short) => toBigNumber(ethers.utils.formatEther(short)))
-			);
+			const rewardsTotalSupplies = stats;
+			const shorts = stats.splice(0, stats.length / 3);
+			const rewardsRates = stats.splice(0, stats.length / 2);
+
+			return currencyKeys.reduce((ret, key, i) => {
+				ret[key] = {
+					shorts: toBigNumber(ethers.utils.formatEther(shorts[i])),
+					rewardsRate: toBigNumber(rewardsRates[i].toString()),
+					rewardsTotalSupply: toBigNumber(rewardsTotalSupplies[i].toString()),
+				};
+				return ret;
+			}, {} as ReturnValueType);
 		},
 		{
 			enabled: isAppReady,
