196416d3f887543f0168304517f493386f1ea393
Dave <dbschwartz17@gmail.com>
2021-06-09 14:09:42 -0700

    fix market cap field (#388)

--- queries/rates/useSynthMarketCapQuery.ts
+++ queries/rates/useSynthMarketCapQuery.ts
@@ -2,6 +2,9 @@ import { QueryConfig, useQuery } from 'react-query';
 import { ethers } from 'ethers';
 import synthetix from 'lib/synthetix';
 import BigNumber from 'bignumber.js';
+import { useRecoilValue } from 'recoil';
+
+import { appReadyState } from 'store/app';
 
 import { CurrencyKey } from 'constants/currency';
 import QUERY_KEYS from 'constants/queryKeys';
@@ -14,17 +17,24 @@ const useSynthMarketCapQuery = (
 	currencyKey: CurrencyKey | null,
 	options?: QueryConfig<BigNumber>
 ) => {
+	const isAppReady = useRecoilValue(appReadyState);
+
 	return useQuery<BigNumber>(
 		QUERY_KEYS.Rates.MarketCap(currencyKey as string),
 		async () => {
-			const totalSupply = ethers.utils.formatEther(
-				await synthetix.js!.contracts[synthToContractName(currencyKey!)].totalSupply()
-			);
+			const data = await Promise.all([
+				synthetix.js!.contracts[synthToContractName(currencyKey as string)].totalSupply(),
+				synthetix.js!.contracts.ExchangeRates.rateForCurrency(
+					synthetix.js!.toBytes32(currencyKey as string)
+				),
+			]);
 
-			return toBigNumber(totalSupply);
+			const [totalSupply, price] = data.map((val) => Number(ethers.utils.formatEther(val)));
+
+			return toBigNumber(totalSupply * price);
 		},
 		{
-			enabled: currencyKey != null,
+			enabled: isAppReady && currencyKey != null,
 			...options,
 		}
 	);
