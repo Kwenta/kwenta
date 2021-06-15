96bf2bb6c1d66044cf84d3400899ed0241c75bd7
Clement Balestrat <clement.balestrat@gmail.com>
2021-06-15 16:32:48 +1000

    Revert "imtoken added"
    This reverts commit 069e11928a22c930e56e48975d94dbbced418c08.

--- containers/Connector/config.ts
+++ containers/Connector/config.ts
@@ -17,7 +17,7 @@ export const initOnboard = (network: Network, subscriptions: Subscriptions) => {
 			wallets: [
 				{
 					name: 'Browser Wallet',
-					iconSrc: '/images/wallet-icons/browserWallet.svg',
+					iconSrc: '/images/wallet-icons/browserWallet.png',
 					type: 'injected',
 					link: 'https://metamask.io',
 					wallet: async (helpers) => {
@@ -33,38 +33,40 @@ export const initOnboard = (network: Network, subscriptions: Subscriptions) => {
 					mobile: true,
 				},
 				{
-					walletName: 'lattice',
-					appName: 'Kwenta',
-					rpcUrl: infuraRpc,
-				},
-				{
 					walletName: 'ledger',
 					rpcUrl: infuraRpc,
 					preferred: true,
+				},
+				{
+					walletName: 'lattice',
+					appName: 'Kwenta',
+					rpcUrl: infuraRpc,
 				},
 				{
 					walletName: 'trezor',
 					appUrl: 'https://www.synthetix.io',
 					email: 'info@synthetix.io',
 					rpcUrl: infuraRpc,
-					preferred: true,
 				},
 				{
 					walletName: 'walletConnect',
 					rpc: { [network.id]: infuraRpc },
 					preferred: true,
 				},
-				{ walletName: 'imToken', rpcUrl: infuraRpc, preferred: true },
+				{ walletName: 'coinbase', preferred: true },
 				{
 					walletName: 'portis',
 					apiKey: process.env.NEXT_PUBLIC_PORTIS_APP_ID,
+					preferred: true,
 				},
-				{ walletName: 'gnosis' },
 				{ walletName: 'trust', rpcUrl: infuraRpc },
-				{ walletName: 'walletLink', rpcUrl: infuraRpc, preferred: true },
+				{ walletName: 'dapper' },
+				{ walletName: 'walletLink', rpcUrl: infuraRpc },
 				{ walletName: 'torus' },
 				{ walletName: 'status' },
+				// { walletName: 'unilogin' },
 				{ walletName: 'authereum' },
+				{ walletName: 'imToken' },
 			],
 		},
 		walletCheck: [
