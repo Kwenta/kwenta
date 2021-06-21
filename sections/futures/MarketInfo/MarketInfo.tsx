820d02c2ca32a9074a6139c67df38440b860ed06
AndyTCF <andy.c.f.98@gmail.com>
2021-06-21 13:43:55 +1000

    add market info

--- sections/futures/MarketInfo/MarketInfo.tsx
+++ sections/futures/MarketInfo/MarketInfo.tsx
@@ -1,16 +1,21 @@
 import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
+import Head from 'next/head';
 import styled from 'styled-components';
 
-import { SYNTHS_MAP } from 'constants/currency';
 import useExchangeRatesQuery from 'queries/rates/useExchangeRatesQuery';
-import { getExchangeRatesForCurrencies } from 'utils/currencies';
-import PriceChartCard from 'sections/futures/Charts/PriceChartCard';
 import useSelectedPriceCurrency from 'hooks/useSelectedPriceCurrency';
+
+import { getExchangeRatesForCurrencies } from 'utils/currencies';
+import { formatCurrency, formatPercent } from 'utils/formatters/number';
+
+import { SYNTHS_MAP } from 'constants/currency';
+
+import PriceChartCard from 'sections/futures/Charts/PriceChartCard';
+
+import { FlexDivCol, FlexDivRow } from 'styles/common';
+import CurrencyIcon from 'components/Currency/CurrencyIcon';
 import Card from 'components/Card';
-import { FlexDivRowCentered } from 'styles/common';
-import Head from 'next/head';
-import { formatCurrency } from 'utils/formatters/number';
 
 type MarketInfoProps = {};
 
@@ -49,13 +54,85 @@ const MarketInfo: React.FC<MarketInfoProps> = ({}) => {
 				alignRight={false}
 			/>
 			<MarketInfoContainer>
-				<StyledFlexDiv></StyledFlexDiv>
+				<StyledFlexDiv>
+					<InfoBox>
+						<InfoTitle>{t('futures.market.info.asset')}</InfoTitle>
+						<InfoData>
+							<StyledCurrencyIcon currencyKey={baseCurrencyKey} />
+							{baseCurrencyKey}
+						</InfoData>
+					</InfoBox>
+					<InfoBox>
+						<InfoTitle>{t('futures.market.info.size')}</InfoTitle>
+						<InfoData>
+							{formatCurrency(selectedPriceCurrency.name, 10000, {
+								currencyKey: selectedPriceCurrency.name,
+							})}
+						</InfoData>
+					</InfoBox>
+					<InfoBox>
+						<InfoTitle>{t('futures.market.info.volume')}</InfoTitle>
+						<InfoData>
+							{formatCurrency(selectedPriceCurrency.name, 200000, {
+								currencyKey: selectedPriceCurrency.name,
+							})}
+						</InfoData>
+					</InfoBox>
+					<InfoBox>
+						<InfoTitle>{t('futures.market.info.skew')}</InfoTitle>
+						<InfoData>
+							{formatCurrency(selectedPriceCurrency.name, 200000, {
+								currencyKey: selectedPriceCurrency.name,
+							})}
+						</InfoData>
+					</InfoBox>
+					<InfoBox>
+						<InfoTitle>{t('futures.market.info.debt')}</InfoTitle>
+						<InfoData>
+							{formatCurrency(selectedPriceCurrency.name, 100000, {
+								currencyKey: selectedPriceCurrency.name,
+							})}
+						</InfoData>
+					</InfoBox>
+					<InfoBox>
+						<InfoTitle>{t('futures.market.info.rate')}</InfoTitle>
+						<InfoData>{formatPercent(0.005)}</InfoData>
+					</InfoBox>
+				</StyledFlexDiv>
 			</MarketInfoContainer>
 		</>
 	);
 };
 export default MarketInfo;
 
-const MarketInfoContainer = styled(Card)``;
+const MarketInfoContainer = styled(Card)`
+	margin: 16px 0px;
+`;
 
-const StyledFlexDiv = styled(FlexDivRowCentered)``;
+const StyledFlexDiv = styled(FlexDivRow)`
+	justify-content: space-evenly;
+	align-items: center;
+`;
+
+const InfoBox = styled(FlexDivCol)`
+	padding: 20px;
+`;
+
+const InfoTitle = styled.div`
+	color: ${(props) => props.theme.colors.blueberry};
+	font-family: ${(props) => props.theme.fonts.regular};
+	font-size: 12px;
+	text-transform: capitalize;
+	margin-bottom: 16px;
+`;
+
+const InfoData = styled(FlexDivRow)`
+	align-items: center;
+	color: ${(props) => props.theme.colors.white};
+	font-family: ${(props) => props.theme.fonts.mono};
+	font-size: 12px;
+`;
+
+const StyledCurrencyIcon = styled(CurrencyIcon)`
+	margin-right: 4px;
+`;
