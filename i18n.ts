45b0901b0deb9f2de26ad952dadad3820d46ca8d
Jeremy Chiaramonte <JeremyChiaramonte@gmail.com>
2021-07-28 17:31:57 -0400

    feat: remove korean language (#470)

--- i18n.ts
+++ i18n.ts
@@ -4,13 +4,11 @@ import { initReactI18next } from 'react-i18next';
 import { Language } from 'translations/constants';
 
 import enTranslation from './translations/en.json';
-import krTranslation from './translations/kr.json';
 
 // TODO: lazy load it
 i18n.use(initReactI18next).init({
 	resources: {
 		[Language.EN]: { translation: enTranslation },
-		[Language.KR]: { translation: krTranslation },
 	},
 	fallbackLng: Language.EN,
 	lng: DEFAULT_LANGUAGE,
--- translations/constants.ts
+++ translations/constants.ts
@@ -1,4 +1,3 @@
 export enum Language {
 	EN = 'en',
-	KR = 'kr',
 }
