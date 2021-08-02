a1a57aced4d5002c76844dbedc3ef9884c5f38c3
Jeremy Chiaramonte <JeremyChiaramonte@gmail.com>
2021-08-02 12:55:05 -0400

    feat: rearranged tokenlist fallbacks to synthetix -> 1inch -> zapper (#477)

--- components/Currency/CurrencyIcon/CurrencyIcon.tsx
+++ components/Currency/CurrencyIcon/CurrencyIcon.tsx
@@ -53,31 +53,7 @@ export const CurrencyIcon: FC<CurrencyIconProps> = ({ currencyKey, type, ...rest
 		...rest,
 	};
 
-	if (
-		ZapperTokenListMap != null &&
-		ZapperTokenListMap[currencyKey] != null &&
-		!firstFallbackError
-	) {
-		return (
-			<TokenIcon
-				src={ZapperTokenListMap[currencyKey].logoURI}
-				onError={() => setFirstFallbackError(true)}
-				{...props}
-			/>
-		);
-	} else if (
-		OneInchTokenListMap != null &&
-		OneInchTokenListMap[currencyKey] != null &&
-		!secondFallbackError
-	) {
-		return (
-			<TokenIcon
-				src={OneInchTokenListMap[currencyKey].logoURI}
-				onError={() => setSecondFallbackError(true)}
-				{...props}
-			/>
-		);
-	} else if (thirdFallbackError) {
+	if (!firstFallbackError) {
 		switch (currencyKey) {
 			case CRYPTO_CURRENCY_MAP.ETH: {
 				return <Img src={ETHIcon} {...props} />;
@@ -93,12 +69,36 @@ export const CurrencyIcon: FC<CurrencyIconProps> = ({ currencyKey, type, ...rest
 								? synthetixTokenListMap[currencyKey].logoURI
 								: getSynthIcon(currencyKey as CurrencyKey)
 						}
-						onError={() => setThirdFallbackError(true)}
+						onError={() => setFirstFallbackError(true)}
 						{...props}
 						alt={currencyKey}
 					/>
 				);
 		}
+	} else if (
+		OneInchTokenListMap != null &&
+		OneInchTokenListMap[currencyKey] != null &&
+		!secondFallbackError
+	) {
+		return (
+			<TokenIcon
+				src={OneInchTokenListMap[currencyKey].logoURI}
+				onError={() => setSecondFallbackError(true)}
+				{...props}
+			/>
+		);
+	} else if (
+		ZapperTokenListMap != null &&
+		ZapperTokenListMap[currencyKey] != null &&
+		!thirdFallbackError
+	) {
+		return (
+			<TokenIcon
+				src={ZapperTokenListMap[currencyKey].logoURI}
+				onError={() => setThirdFallbackError(true)}
+				{...props}
+			/>
+		);
 	} else {
 		return (
 			<Placeholder style={{ width: props.width, height: props.height }}>{currencyKey}</Placeholder>
