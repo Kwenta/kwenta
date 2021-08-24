e739fdd653649c35de61540b4eff3c48ad7b50e1
dbeal <git@dbeal.dev>
2021-08-24 16:02:25 -0700

    add matomo
    for user tracking on our internal tools.
    
    related to https://github.com/Synthetixio/issues/issues/312

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -55,6 +55,20 @@ export default class MyDocument extends Document {
 						type="font/woff2"
 						crossOrigin="anonymous"
 					/>
+					{/* matomo */}
+					<script>{`
+					  var _paq = window._paq = window._paq || [];
+					  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+					  _paq.push(['trackPageView']);
+					  _paq.push(['enableLinkTracking']);
+					  (function() {
+					    var u="https://analytics.synthetix.io/";
+					    _paq.push(['setTrackerUrl', u+'matomo.php']);
+					    _paq.push(['setSiteId', '2']);
+					    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+					    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+					  })();
+					`}</script>
 				</Head>
 				<body>
 					<Main />
