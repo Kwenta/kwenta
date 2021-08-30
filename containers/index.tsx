ded6ba6d1b7d6d3c3b6a53cf31f1361e537ed6d5
Kevin Meraz <kevinemeraz@gmail.com>
2021-08-30 17:52:07 -0500

    remove reduntant provider

--- containers/index.tsx
+++ containers/index.tsx
@@ -14,11 +14,9 @@ export const WithAppContainers: FC<WithAppContainersProps> = ({ children }) => (
 	<Connector.Provider>
 		<BlockExplorer.Provider>
 			<Convert.Provider>
-				<BlockExplorer.Provider>
-					<TransactionNotifier.Provider>
-						<L2Gas.Provider>{children}</L2Gas.Provider>
-					</TransactionNotifier.Provider>
-				</BlockExplorer.Provider>
+				<TransactionNotifier.Provider>
+					<L2Gas.Provider>{children}</L2Gas.Provider>
+				</TransactionNotifier.Provider>
 			</Convert.Provider>
 		</BlockExplorer.Provider>
 	</Connector.Provider>
