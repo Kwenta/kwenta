44129fc718a89de9686d9f37af21b804c47b6506
mzmoen <miles.moen@gmail.com>
2021-09-11 15:10:00 -0400

    First step in making gitHash appear. Need to add to exchange and shorting pages as well

---
+++ sections/shared/Layout/AppLayout/Footer/Footer.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+
+const Footer: FC = () => {
+    const gitID = process.env.GIT_HASH_ID!.toString();
+
+    return (
+        <GitIDFooter>
+            {gitID}
+        </GitIDFooter>
+    );
+};
+
+const GitIDFooter = styled.div`
+	font-family: Akkurat LL TT;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 10px;
+    line-height: 140%;
+    /* identical to box height, or 14px */
+    
+    display: flex;
+    justify-items: center;
+    position: absolute;
+    bottom: 0px;
+    display: grid;
+    width: 100%;
+    margin-bottom: 15px;
+    
+    color: #2B3035;
+`;
+
+export default Footer;
