b72d5e42bf6209d7e8a0d7a960afbd1bb2cdc24a
Jeremy Chiaramonte <JeremyChiaramonte@gmail.com>
2021-09-14 23:07:31 -0400

    Cleaned up linting issues (#48) (#49)
    * branch for adding git hash to pages
    
    * cleaned up CSS
    
    * cleaned up linting issues
    
    * attempt to have commit verified
    
    * attempting verification take 3
    
    Co-authored-by: ZanePseudo <90298419+ZanePseudo@users.noreply.github.com>

--- sections/shared/Layout/AppLayout/GitID/GitID.tsx
+++ sections/shared/Layout/AppLayout/GitID/GitID.tsx
@@ -2,25 +2,21 @@ import { FC } from 'react';
 import styled from 'styled-components';
 
 const GitID: FC = () => {
-    const gitID = process.env.GIT_HASH_ID!.toString();
+	const gitID = process.env.GIT_HASH_ID!.toString();
 
-    return (
-        <GitIDFooter>
-            {gitID}
-        </GitIDFooter>
-    );
+	return <GitIDFooter>{gitID}</GitIDFooter>;
 };
 
 const GitIDFooter = styled.div`
-	font-family: ${props => props.theme.fonts.mono};
-    font-size: 10px;
-    
-    left: 50%;
-    transform: translate(-50%, -50%);
-    position: absolute;
-    bottom: 5px;
-        
-    color: #2B3035;
+	font-family: ${(props) => props.theme.fonts.mono};
+	font-size: 10px;
+
+	left: 50%;
+	transform: translate(-50%, -50%);
+	position: absolute;
+	bottom: 5px;
+
+	color: #2b3035;
 `;
 
 export default GitID;
