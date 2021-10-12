f38a1e2851bcc44d2ed140dc188cde579c9dc059
Kevin Meraz <kevinemeraz@gmail.com>
2021-10-12 13:39:43 -0700

    update logic

--- sections/shared/Layout/AppLayout/Header/states.ts
+++ sections/shared/Layout/AppLayout/Header/states.ts
@@ -2,14 +2,14 @@ import { selector } from 'recoil';
 
 import ROUTES from 'constants/routes';
 import { MENU_LINKS, MenuLink } from './constants';
-import { isL2KovanState } from 'store/wallet';
+import { isL2MainnetState } from 'store/wallet';
 
 export const menuLinksState = selector<MenuLink[]>({
 	key: 'menuLinksState',
 	get: ({ get }) => {
-		const isL2Kovan = get(isL2KovanState);
-		return !isL2Kovan
-			? MENU_LINKS
-			: MENU_LINKS.filter((menuLink: MenuLink) => menuLink.link !== ROUTES.Shorting.Home);
+		const isL2Mainnet = get(isL2MainnetState);
+		return isL2Mainnet
+			? MENU_LINKS.filter((menuLink: MenuLink) => menuLink.link !== ROUTES.Shorting.Home)
+			: MENU_LINKS;
 	},
 });
