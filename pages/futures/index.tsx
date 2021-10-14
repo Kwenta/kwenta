e644cc2f1d87bff6c29c49c697dc3cbe9e4c210f
Kevin Meraz <kevinemeraz@gmail.com>
2021-10-13 22:05:03 -0700

    render different pages based on wallet state and susd balance

--- pages/futures/index.tsx
+++ pages/futures/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Head from 'next/head';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
@@ -12,13 +12,34 @@ import { PageContent, MainContent, RightSideContent, FullHeightContainer } from 
 import { DesktopOnlyView } from 'components/Media';
 
 import Hero from 'sections/futures/Hero';
-import Intro from 'sections/futures/Intro';
+import Splash from 'sections/futures/Onboarding/Splash';
+import Tweet from 'sections/futures/Onboarding/Tweet';
 import Markets from 'sections/futures/Markets';
 import WalletOverview from 'sections/futures/WalletOverview';
 import { FuturesMarket } from 'queries/futures/types';
 
+import { useRecoilValue } from 'recoil';
+
+import { isL2KovanState, isWalletConnectedState, walletAddressState } from 'store/wallet';
+import get from 'lodash/get';
+
+import useSynthetixQueries from '@synthetixio/queries';
+import { zeroBN } from 'utils/formatters/number';
+
+type CurrentPageState = 'splash' | 'tweet' | 'futures' | null;
+
 const Futures: FC = () => {
 	const { t } = useTranslation();
+	const [currentPage, setCurrentPage] = useState<CurrentPageState>(null);
+	const walletAddress = useRecoilValue(walletAddressState);
+	const isWalletConnected = useRecoilValue(isWalletConnectedState);
+	const isL2Kovan = useRecoilValue(isL2KovanState);
+
+	const { useSynthsBalancesQuery } = useSynthetixQueries();
+	const synthsWalletBalancesQuery = useSynthsBalancesQuery(walletAddress, {
+		refetchInterval: 5000,
+	});
+
 	const futuresMarketsQuery = useGetFuturesMarkets();
 	const futuresMarkets = futuresMarketsQuery?.data ?? [];
 
@@ -27,15 +48,40 @@ const Futures: FC = () => {
 	);
 	const futuresMarketsPositions = futuresMarketsPositionQuery?.data ?? null;
 
-	const showIntro = true;
+	useEffect(() => {
+		(async () => {
+			// if network isnt l2 kovan, direct to splash screen
+			if (!isWalletConnected || !isL2Kovan) {
+				setCurrentPage('splash');
+				return;
+			}
+			const sUSDBalance = synthsWalletBalancesQuery.isSuccess
+				? get(synthsWalletBalancesQuery.data, ['balancesMap', 'sUSD', 'balance'], zeroBN)
+				: null;
+			// 	if network is l2 kovan but no susd, send them to tweet screen
+			if (!sUSDBalance?.toNumber()) {
+				setCurrentPage('tweet');
+				return;
+			}
+			// if network is l2 kovan and they have susd, send them to regular dashboard
+			setCurrentPage('futures');
+		})();
+	}, [
+		isL2Kovan,
+		isWalletConnected,
+		synthsWalletBalancesQuery.data,
+		synthsWalletBalancesQuery.isSuccess,
+	]);
 
 	return (
 		<>
 			<Head>
 				<title>{t('futures.page-title')}</title>
 			</Head>
-			{showIntro ? (
-				<Intro />
+			{currentPage === 'splash' ? (
+				<Splash />
+			) : currentPage === 'tweet' ? (
+				<Tweet />
 			) : (
 				<AppLayout>
 					<PageContent>
---
+++ sections/futures/Onboarding/Tweet/Tweet.tsx
@@ -0,0 +1,62 @@
+import { FC, useState } from 'react';
+import { Svg } from 'react-optimized-image';
+import { useRecoilValue } from 'recoil';
+
+import SVGListening from 'assets/svg/futures/listening.svg';
+import SVGTweetBox from 'assets/svg/futures/TweetBox';
+import SVGTwitter from 'assets/svg/futures/twitter.svg';
+
+import { truncatedWalletAddressState, walletAddressState } from 'store/wallet';
+import * as Styled from './styles';
+
+import WalletOptionsModal from 'sections/shared/modals/WalletOptionsModal';
+
+import * as StyledOnboarding from '../styles';
+
+const Tweet: FC = () => {
+	const [walletOptionsModalOpened, setWalletOptionsModalOpened] = useState<boolean>(false);
+
+	const [submitted, setSubmitted] = useState(false);
+	const truncatedWalletAddress = useRecoilValue(truncatedWalletAddressState);
+	const walletAddress = useRecoilValue(walletAddressState);
+
+	function tweet() {
+		setSubmitted(true);
+		window.open(
+			`https://twitter.com/intent/tweet?text=Hey%20%40kwenta_io%2C%20it%27s%20${walletAddress}.%20Let%20me%20in%20to%20the%20L2%20testnet%20trading%20competition!%20%40optimismPBC%20%40synthetix_io%20https%3A%2F%2Fkwenta.io%20%23futuresOnKwentaIsHere`
+		);
+	}
+
+	return (
+		<StyledOnboarding.Root>
+			<Styled.Root>
+				{walletOptionsModalOpened && (
+					<WalletOptionsModal onDismiss={() => setWalletOptionsModalOpened(false)} />
+				)}
+				<Styled.WalletButton
+					size="md"
+					variant="outline"
+					onClick={() => setWalletOptionsModalOpened(true)}
+					data-testid="wallet-btn"
+				>
+					<Styled.ConnectionDot />
+					{truncatedWalletAddress}
+				</Styled.WalletButton>
+				<Styled.Line1>Welcome to Futures on Kwenta</Styled.Line1>
+				<Styled.Line2>Tweet to get started</Styled.Line2>
+				<Styled.Line3>
+					To start trading on L2 you need to claim sUSD OVM tokens. Tweet your wallet address along
+					with #futuresOnKwentaIsHere and start trading now!
+				</Styled.Line3>
+				<SVGTweetBox address={walletAddress || ''} />
+				<Styled.Line4>
+					We recommend using a fresh, new wallet for the competition, as there will be a public
+					leaderboard displayed.
+				</Styled.Line4>
+				{submitted ? <Svg src={SVGListening} /> : <Svg src={SVGTwitter} onClick={() => tweet()} />}
+			</Styled.Root>
+		</StyledOnboarding.Root>
+	);
+};
+
+export default Tweet;
