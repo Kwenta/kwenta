b7af1ee963085c8b70ec9c1680513af32f8eb759
JChiaramonte7 <jeremychiaramonte@gmail.com>
2021-10-19 14:30:13 +0100

    fix: update subgraph link

--- queries/futures/constants.ts
+++ queries/futures/constants.ts
@@ -1,3 +1,4 @@
-export const FUTURES_ENDPOINT = 'https://api.thegraph.com/subgraphs/name/jchiaramonte7/futures';
+export const FUTURES_ENDPOINT =
+	'https://api.thegraph.com/subgraphs/name/jchiaramonte7/futurescompetition';
 
 export const DAY_PERIOD = 24 * 3600;
