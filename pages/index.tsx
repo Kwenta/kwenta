cfec887a6312801e3c0872384eb32d10ce07c250
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-01-04 09:50:09 -0700

    Remove onboarding flow

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,7 @@
 import { FC, useEffect, useState } from 'react';
 import Head from 'next/head';
-import get from 'lodash/get';
 import styled from 'styled-components';
-import { useRecoilValue } from 'recoil';
 import { useTranslation } from 'react-i18next';
-
-// import AppLayout from 'sections/shared/Layout/AppLayout';
-// import Hero from 'sections/futures/Hero';
-// import Markets from 'sections/futures/Markets';
-import Splash from 'sections/futures/Onboarding/Splash';
-import Tweet from 'sections/futures/Onboarding/Tweet';
-// import WalletOverview from 'sections/futures/WalletOverview';
-import useSynthetixQueries from '@synthetixio/queries';
-import { isL2KovanState, isWalletConnectedState, walletAddressState } from 'store/wallet';
-
-// import { PageContent, MainContent, RightSideContent, FullHeightContainer } from 'styles/common';
-// import { DesktopOnlyView } from 'components/Media';
-
-import { zeroBN } from 'utils/formatters/number';
 
 import Loading from 'components/Loading';
 
@@ -35,54 +19,10 @@ import Hero from 'sections/futures/Hero';
 import FuturesDashboardTabs from './futures-dashboard-tabs';
 import Leaderboard from 'sections/leaderboard/Leaderboard';
 import { Subheader } from 'sections/futures/common';
-import useGetRegisteredParticipant from 'queries/futures/useGetRegisteredParticipant';
-
-type CurrentPageState = 'splash' | 'tweet' | 'futures' | null;
 
 const Futures: FC = () => {
 	const { t } = useTranslation();
 	const [isLoading, setLoading] = useState<boolean>(true);
-	const [currentPage, setCurrentPage] = useState<CurrentPageState>(null);
-
-	const walletAddress = useRecoilValue(walletAddressState);
-	const isWalletConnected = useRecoilValue(isWalletConnectedState);
-	const isL2Kovan = useRecoilValue(isL2KovanState);
-
-	const { useSynthsBalancesQuery } = useSynthetixQueries();
-	const synthsWalletBalancesQuery = useSynthsBalancesQuery(walletAddress, {
-		refetchInterval: 5000,
-	});
-
-	const useGetRegisteredParticipantQuery = useGetRegisteredParticipant(walletAddress ?? '');
-	const isRegistered = useGetRegisteredParticipantQuery.data ?? false; // assume not registered
-
-	const sUSDBalance = synthsWalletBalancesQuery.isSuccess
-		? get(synthsWalletBalancesQuery.data, ['balancesMap', 'sUSD', 'balance'], zeroBN)
-		: null;
-
-	useEffect(() => {
-		(async () => {
-			// if network isnt l2 kovan, direct to splash screen
-			if (!isWalletConnected || !isL2Kovan) {
-				setCurrentPage('splash');
-				return;
-			}
-
-			// 	if network is l2 kovan but no susd, send them to tweet screen
-			if (!sUSDBalance?.toNumber() && !isRegistered) {
-				setCurrentPage('tweet');
-				return;
-			}
-			// if network is l2 kovan and they have susd, send them to regular dashboard
-			setCurrentPage('futures');
-		})();
-	}, [
-		isL2Kovan,
-		isWalletConnected,
-		synthsWalletBalancesQuery.data,
-		synthsWalletBalancesQuery.isSuccess,
-		sUSDBalance,
-	]);
 
 	useEffect(() => {
 		setTimeout(() => setLoading(false), 1500);
@@ -95,31 +35,25 @@ const Futures: FC = () => {
 			<Head>
 				<title>{t('futures.page-title')}</title>
 			</Head>
-			{currentPage === 'splash' ? (
-				<Splash />
-			) : currentPage === 'tweet' ? (
-				<Tweet />
-			) : (
-				<AppLayout>
-					<PageContent>
-						<FullHeightContainer>
-							<MainContent>
-								<Hero displayReferBox={false} />
-								<FuturesDashboardTabs />
-							</MainContent>
-							<DesktopOnlyView>
-								<StyledRightSideContent>
-									<HeaderRow>
-										<Subheader>{t('futures.leaderboard.title')}</Subheader>
-									</HeaderRow>
-									<Leaderboard compact />
-									<Markets />
-								</StyledRightSideContent>
-							</DesktopOnlyView>
-						</FullHeightContainer>
-					</PageContent>
-				</AppLayout>
-			)}
+			<AppLayout>
+				<PageContent>
+					<FullHeightContainer>
+						<MainContent>
+							<Hero displayReferBox={false} />
+							<FuturesDashboardTabs />
+						</MainContent>
+						<DesktopOnlyView>
+							<StyledRightSideContent>
+								<HeaderRow>
+									<Subheader>{t('futures.leaderboard.title')}</Subheader>
+								</HeaderRow>
+								<Leaderboard compact />
+								<Markets />
+							</StyledRightSideContent>
+						</DesktopOnlyView>
+					</FullHeightContainer>
+				</PageContent>
+			</AppLayout>
 		</>
 	);
 };
--- sections/futures/Onboarding/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Root = styled.div`
-	height: 100vh;
-	overflow: hidden;
-	position: absolute;
-	width: 100%;
-	z-index: 1;
-`;
