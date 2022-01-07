a5aac1d98d4df0bc6b84863c188680bf6cfc9d48
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-01-07 01:47:06 -0700

    Add hover state for buttons with variants

--- sections/futures/Button/Button.tsx
+++ sections/futures/Button/Button.tsx
@@ -4,14 +4,16 @@ type ButtonProps = {
 	size?: 'sm' | 'md';
 	variant?: 'success' | 'danger';
 	mono?: boolean;
-	active?: boolean;
 };
 
 const Button = styled.button<ButtonProps>`
+	width: 100%;
 	cursor: pointer;
 	position: relative;
-	width: 100%;
+	border-radius: 16px;
 
+	color: #ece8e3;
+	border: 1px solid #ffffff1a;
 	background: linear-gradient(180deg, #39332d 0%, #2d2a28 100%);
 	box-shadow: 0px 0px 20px 0px #ffffff08 inset;
 	box-shadow: 0px 2px 2px 0px #00000040;
@@ -30,76 +32,73 @@ const Button = styled.button<ButtonProps>`
 					font-family: ${(props) => props.theme.fonts.bold};
 			  `}
 
-				${(props) =>
-					props.active &&
-					css`
-						text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.4);
+	${({ variant }) =>
+		!!variant &&
+		css`
+			&:before {
+				content: ' ';
+				position: absolute;
+				z-index: -1;
+				top: -4px;
+				right: -4px;
+				bottom: -4px;
+				left: -4px;
+				border-radius: 18px;
+			}
+
+			&:hover {
+				color: #ece8e3;
+				text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.4);
+
+				&:before {
+					box-shadow: 0px 2px 2px 0px #00000040;
+				}
+			}
+		`};
+
+	${({ variant }) =>
+		!!variant &&
+		(variant === 'success'
+			? css`
+					color: #7fd482;
+					border: 2px solid #7fd482;
+
+					&:hover {
+						background: linear-gradient(
+							180deg,
+							rgba(127, 212, 130, 0.5) 0%,
+							rgba(71, 122, 73, 0.5) 100%
+						);
 
 						&:before {
-							content: '';
-							position: absolute;
-							z-index: -1;
-							top: -4px;
-							right: -4px;
-							bottom: -4px;
-							left: -4px;
-							border-radius: 18px;
+							border: 2px solid rgba(127, 212, 130, 0.2);
 						}
-					`}
-
-	${(props) =>
-		props.active && props.variant === 'success'
-			? css`
-					background: linear-gradient(
-						180deg,
-						rgba(127, 212, 130, 0.5) 0%,
-						rgba(71, 122, 73, 0.5) 100%
-					);
-
-					&:before {
-						border: 2px solid red;
 					}
 			  `
-			: props.variant === 'danger' &&
-			  css`
-					background: linear-gradient(
-						180deg,
-						rgba(239, 104, 104, 0.5) 0%,
-						rgba(116, 56, 56, 0.5) 100%
-					);
+			: css`
+					color: #ef6868;
+					border: 2px solid #ef6868;
 
-					&:before {
-						border: 2px solid red;
+					&:hover {
+						background: linear-gradient(
+							180deg,
+							rgba(239, 104, 104, 0.5) 0%,
+							rgba(116, 56, 56, 0.5) 100%
+						);
+
+						&:before {
+							border: 2px solid rgba(239, 104, 104, 0.2);
+						}
 					}
-			  `};
+			  `)}
 
 	${({ size }) =>
 		!size || size === 'md'
 			? css`
 					height: 55px;
-					border-radius: 16px;
-					font-size: 16px;
 			  `
 			: css`
 					height: 36px;
-					border-radius: 12px;
-					font-size: 15px;
-			  `}
-
-	${({ variant }) =>
-		!variant
-			? css`
-					color: #ece8e3;
-					border: 1px solid #ffffff1a;
-			  `
-			: variant === 'success'
-			? css`
-					color: #7fd482;
-					border: 2px solid #7fd482;
-			  `
-			: css`
-					color: #ef6868;
-					border: 2px solid #ef6868;
 			  `}
 `;
 
