68ee22ef203c86362d0cea957b536374862e53d7
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-01-07 19:43:08 -0700

    Add Input component

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -4,17 +4,18 @@ export const inputCSS = css`
 	width: 100%;
 	min-width: 0;
 	font-family: ${(props) => props.theme.fonts.regular};
-	background-color: ${(props) => props.theme.colors.black};
-	height: 32px;
-	padding: 0 8px;
-	font-size: 14px;
-	border: 0;
-	border-radius: 4px;
-	color: ${(props) => props.theme.colors.white};
-	::placeholder {
-		color: ${(props) => props.theme.colors.silver};
-	}
+	border: 1px solid #ffffff1a;
+	background: linear-gradient(180deg, #1b1b1b 0%, rgba(27, 27, 27, 0.3) 100%);
+	height: 46px;
+	padding: 0 12px;
+	font-size: 18px;
+	border-radius: 16px;
+	color: #ece8e3;
 	outline: none;
+
+	::placeholder {
+		color: #787878;
+	}
 `;
 
 export const Input = styled.input`
---
+++ components/Input/OrderSizingInput.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type OrderSizingInputProps = {
+	value: string;
+	onChange(text: string): void;
+	synth: string;
+	style?: React.CSSProperties;
+};
+
+const OrderSizingInput: React.FC<OrderSizingInputProps> = ({ value, onChange, synth, style }) => {
+	return (
+		<OrderSizingInputContainer {...{ style }}>
+			<input value={value} onChange={(e) => onChange(e.target.value)} />
+			<span>{synth}</span>
+		</OrderSizingInputContainer>
+	);
+};
+
+const OrderSizingInputContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	box-sizing: border-box;
+	height: 46px;
+	background: linear-gradient(180deg, #1b1b1b 0%, rgba(27, 27, 27, 0.75) 100%);
+	box-shadow: 0px 0.5px 0px 0px #ffffff14;
+	border: 1px solid #ffffff1a;
+	border-radius: 16px;
+	padding: 12px 14px;
+
+	input {
+		font-family: ${(props) => props.theme.fonts.mono};
+		font-size: 18px;
+		line-height: 22px;
+		background-color: transparent;
+		border: none;
+		color: #ece8e3;
+
+		&:focus {
+			outline: none;
+		}
+	}
+
+	span {
+		font-family: monospace;
+		font-size: 16px;
+		color: #787878;
+	}
+`;
+
+export default OrderSizingInput;
