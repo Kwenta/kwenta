db2f77d1bf6e8677dd0bb8e24de8f74062fd668c
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-01-17 21:58:13 -0700

    Install Storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
+import '../styles/main.css';
+
+export const parameters = {
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
+
+export const decorators = [
+	(Story) => (
+		<ThemeProvider theme={theme}>
+			<Story />
+		</ThemeProvider>
+	),
+];
