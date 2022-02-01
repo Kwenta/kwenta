133de56ad0049f3e855195dbe41d716595b366d6
kethcode <kethic@kethic.com>
2022-01-31 19:45:04 -0800

    added graceful redirect from L1 to L2 for shorting

--- sections/shorting/ShortingCard/ShortingCard.tsx
+++ sections/shorting/ShortingCard/ShortingCard.tsx
@@ -11,24 +11,43 @@ import useShort from '../hooks/useShort';
 
 import { CurrencyCardsSelector, ExchangeCardsWithSelector } from 'styles/common';
 
+import { useTranslation, Trans } from 'react-i18next';
+import { useRecoilValue } from 'recoil';
+import { isL2State } from 'store/wallet';
+
+import { MessageContainer, Message } from '../common';
+import NetworksSwitcher from 'sections/shared/Layout/AppLayout/Header/NetworksSwitcher';
+
 const ShortingCard: FC = () => {
 	const { quoteCurrencyCard, baseCurrencyCard, footerCard } = useShort({
 		defaultBaseCurrencyKey: Synths.sETH,
 		defaultQuoteCurrencyKey: Synths.sUSD,
 	});
 
+	const { t } = useTranslation();
+	const isL2 = useRecoilValue(isL2State);
+
 	return (
 		<Container>
-			<ConvertContainer>
-				<ExchangeCardsWithSelector>
-					{quoteCurrencyCard}
-					{baseCurrencyCard}
-					<StyledCurrencyCardsSelector>
-						<CRatioSelector />
-					</StyledCurrencyCardsSelector>
-				</ExchangeCardsWithSelector>
-				<ExchangeFooter>{footerCard}</ExchangeFooter>
-			</ConvertContainer>
+			{isL2 ? (
+				<ConvertContainer>
+					<ExchangeCardsWithSelector>
+						{quoteCurrencyCard}
+						{baseCurrencyCard}
+						<StyledCurrencyCardsSelector>
+							<CRatioSelector />
+						</StyledCurrencyCardsSelector>
+					</ExchangeCardsWithSelector>
+					<ExchangeFooter>{footerCard}</ExchangeFooter>
+				</ConvertContainer>
+			) : (
+				<MessageContainer>
+					<Message>
+						<Trans t={t} i18nKey="shorting.l1-deprecated" />
+					</Message>
+					<NetworksSwitcher />
+				</MessageContainer>
+			)}
 		</Container>
 	);
 };
--- sections/shorting/common.tsx
+++ sections/shorting/common.tsx
@@ -1,4 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import { FixedFooterMixin, GridDivCentered } from 'styles/common';
+import media from 'styles/media';
+
+import { zIndex } from 'constants/ui';
 
 export const Title = styled.div`
 	color: ${(props) => props.theme.colors.white};
@@ -6,4 +11,49 @@ export const Title = styled.div`
 	font-size: 14px;
 	text-transform: capitalize;
 	padding-bottom: 15px;
+`;
+
+export const Message = styled.div`
+	color: ${(props) => props.theme.colors.white};
+	font-size: 14px;
+	font-family: ${(props) => props.theme.fonts.bold};
+	flex-grow: 1;
+	text-align: center;
+`;
+
+export const MessageContainer = styled(GridDivCentered)<{
+	attached?: boolean;
+	showProvider?: boolean;
+}>`
+	display: grid;
+	-webkit-box-align: center;
+	align-items: center;
+	width: 100%;
+	border-radius: 4px;
+	grid-template-columns: ${(props) => props.showProvider && '.5fr'} 1fr auto;
+	background-color: ${(props) => props.theme.colors.elderberry};
+	padding: 16px 32px;
+	margin: 0 0 20px;
+
+	/*
+	width: 100%;
+	border-radius: 1000px;
+	grid-template-columns: 1fr auto;
+	background-color: ${(props) => props.theme.colors.elderberry};
+	padding: 16px 32px;
+	max-width: 750px;
+	margin: 0 auto;
+	*/
+	${(props) =>
+		props.attached &&
+		css`
+			border-radius: 4px;
+		`}
+	${media.lessThan('md')`
+		${FixedFooterMixin};
+		box-shadow: 0 -8px 8px 0 ${(props) => props.theme.colors.black};
+		justify-content: center;
+		display: flex;
+		z-index: ${zIndex.BASE};
+	`}
 `;
