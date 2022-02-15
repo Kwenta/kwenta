ec41d8ecaff50965cb9bd77a4e02947b8c265e50
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-02-15 09:53:50 -0700

    Fix utils/etherscan.ts

--- utils/etherscan.ts
+++ utils/etherscan.ts
@@ -4,9 +4,9 @@ import { Network } from 'store/wallet';
 const getEtherscanBaseURL = (network: Network) => {
 	if (network.id === NetworkIdByName.mainnet) {
 		return 'https://etherscan.io';
-	} else if (network.id === NetworkId['Kovan-Ovm']) {
+	} else if (network.id === NetworkIdByName['kovan-ovm']) {
 		return 'https://kovan-optimistic.etherscan.io';
-	} else if (network.id === NetworkId['Mainnet-Ovm']) {
+	} else if (network.id === NetworkIdByName['mainnet-ovm']) {
 		return 'https://optimistic.etherscan.io';
 	}
 	return `https://${network.name}.etherscan.io`;
