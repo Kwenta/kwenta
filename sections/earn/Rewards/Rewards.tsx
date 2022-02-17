79dfe0613f40e4f9b8502eafe7db774909de13d4
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-02-17 12:31:04 -0700

    Rewards styles

--- sections/earn/Rewards/Rewards.tsx
+++ sections/earn/Rewards/Rewards.tsx
@@ -6,23 +6,31 @@ import { KwentaText, Title, Description } from '../common';
 
 const Rewards: React.FC = () => {
 	return (
-		<div style={{ gridArea: 'rsb' }}>
+		<RewardsContainer>
 			<RewardsHeading variant="h5">Kwenta Rewards Total</RewardsHeading>
-			<RewardsContainer>
+			<RewardsBody>
 				<Title>Your Total Rewards</Title>
 				<KwentaText white>734.72</KwentaText>
 				<StyledDescription>Total KWENTA claimable one live.</StyledDescription>
 				<Button disabled>Claim KWENTA</Button>
-			</RewardsContainer>
-		</div>
+			</RewardsBody>
+		</RewardsContainer>
 	);
 };
 
 const RewardsContainer = styled.div`
+	grid-area: rsb;
+`;
+
+const RewardsBody = styled.div`
 	width: 216px;
 	padding: 18px 25px;
 	border: 1px solid #353333;
 	border-radius: 16px;
+
+	& > button:disabled {
+		background-color: transparent;
+	}
 `;
 
 const StyledDescription = styled(Description)`
@@ -31,9 +39,11 @@ const StyledDescription = styled(Description)`
 
 const RewardsHeading = styled(Text.Heading)`
 	color: ${(props) => props.theme.colors.common.primaryGold};
-	margin-bottom: 16px;
+	margin-bottom: 8px;
+	margin-left: 8px;
 	text-transform: uppercase;
 	font-family: ${(props) => props.theme.fonts.bold};
+	font-size: 13px;
 `;
 
 export default Rewards;
