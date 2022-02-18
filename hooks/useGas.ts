5c541d0104443764f00b97186677627ebc4b34b4
JChiaramonte7 <jeremychiaramonte@gmail.com>
2022-02-18 16:36:39 -0500

    refactor: removed unused import

--- hooks/useGas.ts
+++ hooks/useGas.ts
@@ -6,7 +6,6 @@ import useSynthetixQueries, { GasPrice } from '@synthetixio/queries';
 import { customGasPriceState, gasSpeedState } from 'store/wallet';
 import { normalizeGasLimit, gasPriceInWei } from 'utils/network';
 import { wei } from '@synthetixio/wei';
-import { GWEI_UNIT } from 'constants/network';
 
 // TODO add support for 1559. For now use maxFeePerGas (legacy)
 export const parseGasPriceObject = (gasPriceObject: GasPrice): number | null => {
