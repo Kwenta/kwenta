5a04a05ca6cb68af3f466c8abb1f4de48bc8b68b
JChiaramonte7 <jeremychiaramonte@gmail.com>
2022-02-18 13:48:39 -0500

    feat: enabled sSOL shorting

--- sections/shorting/constants.ts
+++ sections/shorting/constants.ts
@@ -1,3 +1,3 @@
 import { CurrencyKey, Synths } from 'constants/currency';
 
-export const SYNTHS_TO_SHORT: CurrencyKey[] = [Synths.sBTC, Synths.sETH, Synths.sLINK];
+export const SYNTHS_TO_SHORT: CurrencyKey[] = [Synths.sBTC, Synths.sETH, Synths.sLINK, Synths.sSOL];
