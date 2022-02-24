7f281752e217d958deae492dba6abc11ba2453f0
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-02-23 20:03:14 -0700

    Remove new synths from short assets list

--- sections/shorting/constants.ts
+++ sections/shorting/constants.ts
@@ -1,12 +1,4 @@
 import { CurrencyKey, Synths } from 'constants/currency';
 
 export const SYNTHS_TO_SHORT_L1: CurrencyKey[] = [Synths.sBTC, Synths.sETH, Synths.sLINK];
-export const SYNTHS_TO_SHORT: CurrencyKey[] = [
-	Synths.sBTC,
-	Synths.sETH,
-	Synths.sLINK,
-	Synths.sSOL,
-	Synths.sMATIC,
-	Synths.sAVAX,
-	Synths.sEUR,
-];
+export const SYNTHS_TO_SHORT: CurrencyKey[] = [Synths.sBTC, Synths.sETH, Synths.sLINK, Synths.sSOL];
