8ffdab51c507ace209937ec93e1f1d2e9270473f
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-02-23 19:37:01 -0700

    Add sAVAX, sMATIC and sEUR to shorts list

--- sections/shorting/constants.ts
+++ sections/shorting/constants.ts
@@ -1,4 +1,12 @@
 import { CurrencyKey, Synths } from 'constants/currency';
 
 export const SYNTHS_TO_SHORT_L1: CurrencyKey[] = [Synths.sBTC, Synths.sETH, Synths.sLINK];
-export const SYNTHS_TO_SHORT: CurrencyKey[] = [Synths.sBTC, Synths.sETH, Synths.sLINK, Synths.sSOL];
+export const SYNTHS_TO_SHORT: CurrencyKey[] = [
+	Synths.sBTC,
+	Synths.sETH,
+	Synths.sLINK,
+	Synths.sSOL,
+	Synths.sMATIC,
+	Synths.sAVAX,
+	Synths.sEUR,
+];
