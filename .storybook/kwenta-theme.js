0a6f518b0708ed649fd8a506357b003703fceb66
Troy <troy.burmeister@gmail.com>
2022-03-03 14:00:47 -0700

    delete storybook

--- .storybook/kwenta-theme.js
+++
@@ -1,6 +0,0 @@
-import { create } from '@storybook/theming';
-
-export default create({
-  base: 'dark',
-  appContentBg: '#282626'
-});
--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons';
-import kwentaTheme from "./kwenta-theme";
-
-addons.setConfig({
-  theme: kwentaTheme,
-});
