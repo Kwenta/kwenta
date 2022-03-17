1baeb0dd9ce6ee1d78330efad6033b5cbc8f2afc
Troy <troy.burmeister@gmail.com>
2022-03-17 17:44:17 -0600

    edit color

--- sections/futures/MarketDetails/MarketDetails.tsx
+++ sections/futures/MarketDetails/MarketDetails.tsx
@@ -190,11 +190,11 @@ const MarketDetailsContainer = styled.div`
 	}
 
 	.green {
-		color: ${(props) => props.theme.colors.common.primaryGreen};
+		color: ${(props) => props.theme.colors.green};
 	}
 
 	.red {
-		color: ${(props) => props.theme.colors.common.primaryRed};
+		color: ${(props) => props.theme.colors.red};
 	}
 `;
 
