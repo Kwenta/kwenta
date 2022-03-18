4c3203cfa8a3a06f2114b9d6065b631e05841fbe
JChiaramonte7 <jeremychiaramonte@gmail.com>
2022-03-17 23:58:26 -0400

    feat: beta logo

--- sections/shared/Layout/Logo/Logo.tsx
+++ sections/shared/Layout/Logo/Logo.tsx
@@ -6,7 +6,7 @@ import ROUTES from 'constants/routes';
 
 import LogoSvg from 'assets/svg/brand/logo.svg';
 import LogoSvgL2 from 'assets/svg/brand/logol2.svg';
-import LogoSvgFutures from 'assets/svg/brand/logo-l2-futures.svg';
+import LogoSvgBeta from 'assets/svg/brand/logo-beta.svg';
 
 type LogoProps = {
 	isL2: boolean;
@@ -15,7 +15,7 @@ type LogoProps = {
 
 const SvgLogo = ({ isFutures = false, isL2 }: LogoProps) => {
 	if (isFutures) {
-		return <Svg src={LogoSvgFutures} />;
+		return <Svg src={LogoSvgBeta} />;
 	}
 	return isL2 ? <Svg src={LogoSvgL2} /> : <Svg src={LogoSvg} />;
 };
