0718c0996beaeafaec572a03e73e0130c3c39c80
troyb.eth <troy.burmeister@ibotta.com>
2022-03-21 12:02:44 -0600

    remove currency selection (#468)

--- sections/shared/modals/SettingsModal/constants.ts
+++ sections/shared/modals/SettingsModal/constants.ts
@@ -1,12 +1,6 @@
-import PriceCurrencySelect from 'sections/shared/components/PriceCurrencySelect';
 import LanguageSelect from 'sections/shared/components/LanguageSelect';
 
 export const OPTIONS = [
-	{
-		id: 'currency-options',
-		label: 'modals.settings.options.currency',
-		SelectComponent: PriceCurrencySelect,
-	},
 	{
 		id: 'language-options',
 		label: 'modals.settings.options.language',
