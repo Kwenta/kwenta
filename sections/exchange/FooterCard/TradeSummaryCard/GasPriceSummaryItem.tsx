2f841c22082edaae7cea8a43a992850271b0c169
keybig <100385397+keybig@users.noreply.github.com>
2022-03-30 14:15:40 -0400

    gas price summary component update re status l1/l2 (#513)
    * gas price summary component update re status l1/l2
    
    * update gas price summary to only show max-fee on mainnet

--- sections/exchange/FooterCard/TradeSummaryCard/GasPriceSummaryItem.tsx
+++ sections/exchange/FooterCard/TradeSummaryCard/GasPriceSummaryItem.tsx
@@ -66,7 +66,7 @@ const GasPriceSummaryItem: FC<GasPriceSummaryItemProps> = ({
 	return (
 		<SummaryItem {...rest}>
 			<SummaryItemLabel>
-				{isMainnet || !isCustomGasPrice
+				{isMainnet
 					? t('exchange.summary-info.max-fee-gwei')
 					: t('exchange.summary-info.gas-price-gwei')}
 			</SummaryItemLabel>
