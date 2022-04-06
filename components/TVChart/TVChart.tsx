d0beb1d1c51d6f25a8d632c5e0c34cbfe6100ced
Troy <troy.burmeister@gmail.com>
2022-04-06 11:38:41 -0600

    choose resolution

--- components/TVChart/TVChart.tsx
+++ components/TVChart/TVChart.tsx
@@ -61,7 +61,6 @@ export function TVChart({
 			},
 			toolbar_bg: colors.selectedTheme.background,
 			time_frames: [
-				{ text: '3y', resolution: '1W', description: '3 Years' },
 				{ text: '1y', resolution: '1D', description: '1 Year' },
 				{ text: '6m', resolution: '1D', description: '6 Months' },
 				{ text: '3m', resolution: '1D', description: '3 Months' },
--- queries/rates/useCandlesticksQuery.ts
+++ queries/rates/useCandlesticksQuery.ts
@@ -1,15 +1,21 @@
 import request, { gql } from 'graphql-request';
+import { ResolutionString } from 'public/static/charting_library/charting_library';
 import { Candle } from './types';
 
 export const requestCandlesticks = async (
 	currencyKey: string | null,
 	minTimestamp: number,
 	maxTimestamp = Math.floor(Date.now() / 1000),
+	resolution: ResolutionString,
 	isL2 = false,
 ) => {
 	const RATES_ENDPOINT = isL2
 		? 'https://api.thegraph.com/subgraphs/name/synthetixio-team/optimism-main'
 		: 'https://api.thegraph.com/subgraphs/name/synthetixio-team/mainnet-main';
+
+	const period = 
+		resolution === '60' ? 3600 :
+		resolution === '1D' ? 86400 : 3600;
 
 	const response = (await request(
 		RATES_ENDPOINT,
@@ -39,7 +45,7 @@ export const requestCandlesticks = async (
 			synth: currencyKey,
 			maxTimestamp: maxTimestamp,
 			minTimestamp: minTimestamp,
-			period: 3600
+			period: period
 		}
 	)) as {
 		[key: string]: Array<Candle>;
