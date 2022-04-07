553ad1432173ba89fc92b9e0565ec2258455b8e1
Yadong Zhang <zhyd007@gmail.com>
2022-04-08 05:34:34 +0800

    refactor: removed unused L2Gas.

--- containers/index.tsx
+++ containers/index.tsx
@@ -3,7 +3,6 @@ import React, { FC } from 'react';
 import Connector from './Connector';
 import BlockExplorer from './BlockExplorer';
 import TransactionNotifier from './TransactionNotifier';
-import L2Gas from './L2Gas';
 import Convert from './Convert';
 
 type WithAppContainersProps = {
@@ -15,7 +14,7 @@ export const WithAppContainers: FC<WithAppContainersProps> = ({ children }) => (
 		<BlockExplorer.Provider>
 			<Convert.Provider>
 				<TransactionNotifier.Provider>
-					<L2Gas.Provider>{children}</L2Gas.Provider>
+					{children}
 				</TransactionNotifier.Provider>
 			</Convert.Provider>
 		</BlockExplorer.Provider>
