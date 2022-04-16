ab70d715745f92e28d2010e26bbb2d924c05ccb8
asherism <asherism@users.noreply.github.com>
2022-04-16 13:43:18 -0300

    border mixin

--- styles/common.tsx
+++ styles/common.tsx
@@ -426,3 +426,20 @@ export const ExchangeCardsWithSelector = styled.div`
 		}
 	}
 `;
+
+export const border = css`
+	&::before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		border-radius: 10px;
+		padding: 1px;
+		background: rgb(255 255 255 / 10%);
+		-webkit-mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+		-webkit-mask-composite: xor;
+		mask-composite: exclude;
+	}
+`;
