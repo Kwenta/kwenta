53913f638e8bbcb29a1a0190c60b032e8e3944f7
Brian Rossetti <brossetti1@gmail.com>
2022-04-25 12:25:38 -0600

    fixing lints

--- sections/futures/Trade/MarketsDropdown.tsx
+++ sections/futures/Trade/MarketsDropdown.tsx
@@ -116,6 +116,7 @@ const MarketsDropdown: React.FC<Props> = ({ asset }) => {
 				futuresClosureReason
 			);
 		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [
 		dailyPriceChangesQuery?.data,
 		futuresMarketsQuery?.data,
