2263d579b315beb29b36b8d050c720c0781949ba
Leifu Chen <leifu.chen@gmail.com>
2022-04-27 21:03:36 +0300

    Disable the place order button when market cap reached

--- sections/futures/Trade/Trade.tsx
+++ sections/futures/Trade/Trade.tsx
@@ -25,7 +25,6 @@ import useGetFuturesMarkets from 'queries/futures/useGetFuturesMarkets';
 import useGetFuturesPositionHistory from 'queries/futures/useGetFuturesMarketPositionHistory';
 import useExchangeRatesQuery from 'queries/rates/useExchangeRatesQuery';
 import MarketsDropdown from './MarketsDropdown';
-// import SegmentedControl from 'components/SegmentedControl';
 import PositionButtons from '../PositionButtons';
 import OrderSizing from '../OrderSizing';
 import MarketInfoBox from '../MarketInfoBox/MarketInfoBox';
@@ -36,6 +35,7 @@ import { getFuturesMarketContract } from 'queries/futures/utils';
 import Connector from 'containers/Connector';
 import { getMarketKey } from 'utils/futures';
 import useFuturesMarketClosed from 'hooks/useFuturesMarketClosed';
+import useGetFuturesMarketLimit from 'queries/futures/useGetFuturesMarketLimit';
 
 const DEFAULT_MAX_LEVERAGE = wei(10);
 
@@ -53,6 +53,7 @@ const Trade: React.FC = () => {
 	const { isFuturesMarketClosed } = useFuturesMarketClosed(marketAsset);
 	const marketQuery = useGetFuturesMarkets();
 	const market = marketQuery?.data?.find(({ asset }) => asset === marketAsset) ?? null;
+	const marketLimitQuery = useGetFuturesMarketLimit(getMarketKey(marketAsset, network.id));
 
 	const futuresPositionHistoryQuery = useGetFuturesPositionHistory(marketAsset);
 	const futuresMarketPositionQuery = useGetFuturesPositionForMarket(
@@ -105,6 +106,14 @@ const Trade: React.FC = () => {
 			return positionLeverage.add(marketMaxLeverage);
 		}
 	}, [positionLeverage, positionSide, leverageSide, marketMaxLeverage]);
+
+	const maxMarketValueUSD = marketLimitQuery?.data ?? wei(0);
+	const marketSize = market?.marketSize ?? wei(0);
+
+	const isMarketCapReached = useMemo(
+		() => marketSize?.mul(marketAssetRate ?? wei(0)).gte(maxMarketValueUSD),
+		[marketSize, marketAssetRate, maxMarketValueUSD]
+	);
 
 	const onTradeAmountChange = React.useCallback(
 		(value: string, fromLeverage: boolean = false) => {
@@ -178,8 +187,10 @@ const Trade: React.FC = () => {
 		return !futuresMarketsPosition?.remainingMargin ||
 			futuresMarketsPosition.remainingMargin.lt('50')
 			? 'futures.market.trade.button.deposit-margin-minimum'
+			: isMarketCapReached
+			? 'futures.market.trade.button.market-caps-reached'
 			: 'futures.market.trade.button.open-position';
-	}, [futuresMarketsPosition]);
+	}, [futuresMarketsPosition, isMarketCapReached]);
 
 	useEffect(() => {
 		const getOrderFee = async () => {
@@ -287,8 +298,6 @@ const Trade: React.FC = () => {
 				isMarketClosed={isFuturesMarketClosed}
 			/>
 
-			{/* <StyledSegmentedControl values={['Market', 'Limit']} selectedIndex={0} onChange={() => {}} /> */}
-
 			<PositionButtons
 				selected={leverageSide}
 				onSelect={setLeverageSide}
@@ -327,7 +336,8 @@ const Trade: React.FC = () => {
 					sizeDelta.eq(zeroBN) ||
 					!!error ||
 					placeOrderTranslationKey === 'futures.market.trade.button.deposit-margin-minimum' ||
-					isFuturesMarketClosed
+					isFuturesMarketClosed ||
+					isMarketCapReached
 				}
 				onClick={() => {
 					setIsTradeConfirmationModalOpen(true);
