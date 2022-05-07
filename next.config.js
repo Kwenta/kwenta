36fb1ee4c56b9c01eeb8f8174d865165c2813fe4
Yadong Zhang <zhyd007@gmail.com>
2022-05-07 18:29:47 +0800

    chore: intercepted stdout warnings.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
-// next.config.js
+/**
+ * @type {import('next').NextConfig}
+ */
 const gitRevision = require('child_process')
 	.execSync('git rev-parse --short HEAD')
 	.toString()
 	.trim();
+
+// https://github.com/facebookexperimental/Recoil/issues/733#issuecomment-925072943
+const intercept = require('intercept-stdout');
+
+// safely ignore recoil stdout warning messages
+function interceptStdout(text) {
+	if (text.includes('Duplicate atom key')) {
+		return '';
+	}
+	return text;
+}
+
+// Intercept in dev and prod
+intercept(interceptStdout);
 
 module.exports = {
 	env: {
