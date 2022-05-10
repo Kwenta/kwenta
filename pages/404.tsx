563f365a40e6184adf0e9b377871a741e078b0ee
Brian Rossetti <brossetti1@gmail.com>
2022-05-10 12:57:37 -0600

    testing moving layout up one layer

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,8 +3,6 @@ import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { Svg } from 'react-optimized-image';
 
-import AppLayout from 'sections/shared/Layout/AppLayout';
-
 import media from 'styles/media';
 import { FlexDivRowCentered } from 'styles/common';
 
@@ -19,16 +17,14 @@ const NotFoundPage = () => {
 			<Head>
 				<title>{t('not-found.page-title')}</title>
 			</Head>
-			<AppLayout>
-				<Container>
-					<Svg src={CaretLeftXLIcon} />
-					<Content>
-						<Title>{t('not-found.title')}</Title>
-						<Subtitle>{t('not-found.subtitle')}</Subtitle>
-					</Content>
-					<Svg src={CaretRightXLICon} />
-				</Container>
-			</AppLayout>
+			<Container>
+				<Svg src={CaretLeftXLIcon} />
+				<Content>
+					<Title>{t('not-found.title')}</Title>
+					<Subtitle>{t('not-found.subtitle')}</Subtitle>
+				</Content>
+				<Svg src={CaretRightXLICon} />
+			</Container>
 		</>
 	);
 };
