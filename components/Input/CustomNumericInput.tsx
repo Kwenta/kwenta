9def23d7ada9a751b1b093bec4f1da8c86a42269
léo <28714795+leovct@users.noreply.github.com>
2022-05-11 04:45:51 +0200

    Improve input fields (#694)
    * improve input fields
    
    * fix leverage input placeholder
    
    * fix issue with leverage input decimals when clicking on the max button
    
    * fix issue with custom amount inputs in order size section
    
    * fix of the previous fix
    
    * fix leverage input behaviour when using leverage buttons or slide

---
+++ components/Input/CustomNumericInput.tsx
@@ -0,0 +1,86 @@
+import { ChangeEvent, FC } from 'react';
+import styled from 'styled-components';
+import Input from './Input';
+
+type CustomNumericInputProps = {
+	value: string;
+	placeholder?: string;
+	suffix: string;
+	onChange: (e: ChangeEvent<HTMLInputElement>, value: string) => void;
+	className?: string;
+	defaultValue?: any;
+	maxValue?: number;
+	disabled?: boolean;
+	id?: string;
+};
+
+const CustomNumericInput: FC<CustomNumericInputProps> = ({
+	value,
+	placeholder,
+	suffix,
+	onChange,
+	className,
+	defaultValue,
+	maxValue,
+	disabled,
+	id,
+	...rest
+}) => {
+	const style = {
+		'--numchs': value.length,
+		'--suffix': `'${value.length === 0 ? '' : suffix}'`,
+	};
+
+	const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const { value } = e.target;
+		const max = maxValue || 0;
+		const valueIsAboveMax = max !== 0 && Number(value) > max;
+		if (!valueIsAboveMax) {
+			onChange(
+				e,
+				value
+					.replace(/[^0-9.,]/g, '')
+					.replace(/,/g, '.')
+					.substring(0, 4)
+			);
+		}
+	};
+
+	return (
+		<InputWrapper style={style as React.CSSProperties}>
+			<StyledInput
+				type="text"
+				value={value}
+				placeholder={placeholder + suffix}
+				onChange={handleOnChange}
+				className={className}
+				defaultValue={defaultValue}
+				disabled={disabled}
+				id={id}
+				{...rest}
+			/>
+		</InputWrapper>
+	);
+};
+
+export const InputWrapper = styled.div`
+	position: relative;
+	overflow: hidden;
+	::after {
+		position: absolute;
+		top: calc(25%);
+		left: calc((var(--numchs) * 1ch + 1.3ch));
+		//left: 5.3ch;
+		content: var(--suffix, 'x');
+		font-family: ${(props) => props.theme.fonts.mono};
+		font-size: 18px;
+		color: ${(props) => props.theme.colors.selectedTheme.input.placeholder};
+	}
+`;
+
+export const StyledInput = styled(Input)`
+	font-family: ${(props) => props.theme.fonts.mono};
+	text-overflow: ellipsis;
+`;
+
+export default CustomNumericInput;
