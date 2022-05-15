9bcba756a8188fb3588eaf9bee0767033368016f
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-05-15 03:05:55 -0400

    Start adding menu logic

--- sections/shared/Layout/AppLayout/Header/MobileUserMenu/MobileUserMenu.tsx
+++ sections/shared/Layout/AppLayout/Header/MobileUserMenu/MobileUserMenu.tsx
@@ -1,67 +1,28 @@
-import { FC, useState } from 'react';
+import { FC, useState, useReducer } from 'react';
 import styled from 'styled-components';
-import { useRecoilState, useRecoilValue } from 'recoil';
 
-import { isWalletConnectedState } from 'store/wallet';
-import { hasOrdersNotificationState } from 'store/ui';
-import { FixedFooterMixin, resetButtonCSS } from 'styles/common';
+import { FixedFooterMixin } from 'styles/common';
 
-import NotificationIcon from 'assets/svg/app/notification.svg';
-import NotificationAlertIcon from 'assets/svg/app/notification-alert.svg';
 import MenuIcon from 'assets/svg/app/menu.svg';
-import CrossIcon from 'assets/svg/app/cross.svg';
-
 import CloseIcon from 'assets/svg/app/close.svg';
-
-import NotificationsModal from 'sections/shared/modals/NotificationsModal';
 
 import MobileSettingsModal from './MobileSettingsModal';
 import WalletButton from '../UserMenu/WalletButton';
 
 const MobileUserMenu: FC = () => {
-	// const isWalletConnected = useRecoilValue(isWalletConnectedState);
-	const [settingsModalOpened, setSettingsModalOpened] = useState<boolean>(false);
-	const [notificationsModalOpened, setNotificationsModalOpened] = useState<boolean>(false);
-	// const [hasOrdersNotification, setHasOrdersNotification] = useRecoilState(
-	// 	hasOrdersNotificationState
-	// );
+	const [isModalOpen, toggleModalOpen] = useReducer((s) => !s, false);
+	const [settingsModalOpened, setSettingsModalOpened] = useState(false);
 
 	return (
 		<>
 			<MobileFooterContainer>
-				<CloseIcon />
+				<div onClick={toggleModalOpen}>{isModalOpen ? <CloseIcon /> : <MenuIcon />}</div>
 				<MobileFooterSeparator />
 				<MobileFooterRight>
 					<div className="text">Menu</div>
 					<WalletButton />
 				</MobileFooterRight>
-				{/* {isWalletConnected && (
-						<MenuButton
-							onClick={() => {
-								setNotificationsModalOpened(!notificationsModalOpened);
-								setSettingsModalOpened(false);
-								if (hasOrdersNotification) {
-									setHasOrdersNotification(false);
-								}
-							}}
-							isActive={notificationsModalOpened}
-						>
-							{hasOrdersNotification ? <NotificationAlertIcon /> : <NotificationIcon />}
-						</MenuButton>
-					)}
-					<MenuButton
-						onClick={() => {
-							setSettingsModalOpened(!settingsModalOpened);
-							setNotificationsModalOpened(false);
-						}}
-						isActive={settingsModalOpened}
-					>
-						{settingsModalOpened ? <CrossIcon /> : <MenuIcon />}
-					</MenuButton> */}
 			</MobileFooterContainer>
-			{notificationsModalOpened && (
-				<NotificationsModal onDismiss={() => setNotificationsModalOpened(false)} />
-			)}
 			{settingsModalOpened && (
 				<MobileSettingsModal onDismiss={() => setSettingsModalOpened(false)} />
 			)}
@@ -97,22 +58,6 @@ const MobileFooterRight = styled.div`
 		color: ${(props) => props.theme.colors.common.primaryWhite};
 		font-family: ${(props) => props.theme.fonts.bold};
 	}
-`;
-
-const Menu = styled.div`
-	padding-right: 26px;
-	display: grid;
-	grid-gap: 10px;
-	grid-auto-flow: column;
-`;
-
-const MenuButton = styled.button<{ isActive: boolean }>`
-	${resetButtonCSS};
-	color: ${(props) => (props.isActive ? props.theme.colors.white : props.theme.colors.blueberry)};
-	&:hover {
-		color: ${(props) => props.theme.colors.white};
-	}
-	padding: 5px;
 `;
 
 export default MobileUserMenu;
