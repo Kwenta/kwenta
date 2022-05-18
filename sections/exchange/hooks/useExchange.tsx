db5d332aa750c0623ff8361ae4d07862711b3532
leifu <leifu.chen@gmail.com>
2022-05-18 20:11:03 +0300

    Fix the broken input issue for into field (#852)
    * Fix the broken input issue for into field
    
    * Fixed auto re-calculation after changing the currency key

--- sections/exchange/hooks/useExchange.tsx
+++ sections/exchange/hooks/useExchange.tsx
@@ -71,6 +71,7 @@ import { useGetL1SecurityFee } from 'hooks/useGetL1SecurityGasFee';
 import useGas from 'hooks/useGas';
 import { KWENTA_TRACKING_CODE } from 'queries/futures/constants';
 import useExchangeFeeRateQuery from 'queries/synths/useExchangeFeeRateQuery';
+import { DEFAULT_CRYPTO_DECIMALS } from 'constants/defaults';
 
 type ExchangeCardProps = {
 	defaultBaseCurrencyKey?: string | null;
@@ -492,7 +493,6 @@ const useExchange = ({
 			: false;
 
 	const handleCurrencySwap = () => {
-		const baseAmount = baseCurrencyAmount;
 		const quoteAmount = quoteCurrencyAmount;
 
 		setCurrencyPair({
@@ -501,7 +501,7 @@ const useExchange = ({
 		});
 
 		setBaseCurrencyAmount(quoteAmount);
-		setQuoteCurrencyAmount(baseAmount);
+		setQuoteCurrencyAmount('');
 
 		if (quoteCurrencyKey != null && baseCurrencyKey != null) {
 			routeToMarketPair(quoteCurrencyKey, baseCurrencyKey);
@@ -562,19 +562,24 @@ const useExchange = ({
 		}
 		if (txProvider === 'synthetix' && quoteCurrencyAmount !== '' && baseCurrencyKey != null) {
 			const baseCurrencyAmountNoFee = wei(quoteCurrencyAmount).mul(rate);
-			const fee = baseCurrencyAmountNoFee.mul(exchangeFeeRate ?? 1);
-			setBaseCurrencyAmount(baseCurrencyAmountNoFee.sub(fee).toString());
+			const fee = baseCurrencyAmountNoFee.mul(exchangeFeeRate ?? 0);
+			setBaseCurrencyAmount(
+				baseCurrencyAmountNoFee.sub(fee).toNumber().toFixed(DEFAULT_CRYPTO_DECIMALS).toString()
+			);
 		}
-	}, [
-		rate,
-		baseCurrencyKey,
-		quoteCurrencyAmount,
-		baseCurrencyAmount,
-		exchangeFeeRate,
-		txProvider,
-		oneInchQuoteQuery.data,
-		oneInchQuoteQuery.isSuccess,
-	]);
+		// eslint-disable-next-line
+	}, [quoteCurrencyKey, exchangeFeeRate, oneInchQuoteQuery.isSuccess, oneInchQuoteQuery.data]);
+
+	useEffect(() => {
+		if (txProvider === 'synthetix' && baseCurrencyAmount !== '' && quoteCurrencyKey != null) {
+			const quoteCurrencyAmountNoFee = wei(baseCurrencyAmount).mul(inverseRate);
+			const fee = quoteCurrencyAmountNoFee.mul(exchangeFeeRate ?? 0);
+			setQuoteCurrencyAmount(
+				quoteCurrencyAmountNoFee.add(fee).toNumber().toFixed(DEFAULT_CRYPTO_DECIMALS).toString()
+			);
+		}
+		// eslint-disable-next-line
+	}, [baseCurrencyKey, exchangeFeeRate]);
 
 	const getExchangeParams = useCallback(
 		(isAtomic: boolean) => {
@@ -936,12 +941,19 @@ const useExchange = ({
 				onAmountChange={async (value) => {
 					if (value === '') {
 						setQuoteCurrencyAmount('');
+						setBaseCurrencyAmount('');
 					} else {
 						setQuoteCurrencyAmount(value);
 						if (txProvider === 'synthetix' && baseCurrencyKey != null) {
 							const baseCurrencyAmountNoFee = wei(value).mul(rate);
-							const fee = baseCurrencyAmountNoFee.mul(exchangeFeeRate ?? 1);
-							setBaseCurrencyAmount(baseCurrencyAmountNoFee.sub(fee).toString());
+							const fee = baseCurrencyAmountNoFee.mul(exchangeFeeRate ?? 0);
+							setBaseCurrencyAmount(
+								baseCurrencyAmountNoFee
+									.sub(fee)
+									.toNumber()
+									.toFixed(DEFAULT_CRYPTO_DECIMALS)
+									.toString()
+							);
 						}
 					}
 				}}
@@ -951,14 +963,22 @@ const useExchange = ({
 						if (quoteCurrencyKey === 'ETH') {
 							const ETH_TX_BUFFER = 0.1;
 							const balanceWithBuffer = quoteCurrencyBalance.sub(wei(ETH_TX_BUFFER));
-							setQuoteCurrencyAmount(balanceWithBuffer.lt(0) ? '0' : balanceWithBuffer.toString());
+							setQuoteCurrencyAmount(
+								balanceWithBuffer.lt(0)
+									? '0'
+									: balanceWithBuffer.toFixed(DEFAULT_CRYPTO_DECIMALS).toString()
+							);
 						} else {
-							setQuoteCurrencyAmount(quoteCurrencyBalance.toString());
+							setQuoteCurrencyAmount(
+								quoteCurrencyBalance.toFixed(DEFAULT_CRYPTO_DECIMALS).toString()
+							);
 						}
 						if (txProvider === 'synthetix') {
 							const baseCurrencyAmountNoFee = quoteCurrencyBalance.mul(rate);
-							const fee = baseCurrencyAmountNoFee.mul(exchangeFeeRate ?? 1);
-							setBaseCurrencyAmount(baseCurrencyAmountNoFee.sub(fee).toString());
+							const fee = baseCurrencyAmountNoFee.mul(exchangeFeeRate ?? 0);
+							setBaseCurrencyAmount(
+								baseCurrencyAmountNoFee.sub(fee).toFixed(DEFAULT_CRYPTO_DECIMALS).toString()
+							);
 						}
 					}
 				}}
@@ -1032,12 +1052,19 @@ const useExchange = ({
 				onAmountChange={async (value) => {
 					if (value === '') {
 						setBaseCurrencyAmount('');
+						setQuoteCurrencyAmount('');
 					} else {
 						setBaseCurrencyAmount(value);
 						if (txProvider === 'synthetix' && baseCurrencyKey != null) {
 							const quoteCurrencyAmountNoFee = wei(value).mul(inverseRate);
-							const fee = quoteCurrencyAmountNoFee.mul(exchangeFeeRate ?? 1);
-							setQuoteCurrencyAmount(quoteCurrencyAmountNoFee.add(fee).toString());
+							const fee = quoteCurrencyAmountNoFee.mul(exchangeFeeRate ?? 0);
+							setQuoteCurrencyAmount(
+								quoteCurrencyAmountNoFee
+									.add(fee)
+									.toNumber()
+									.toFixed(DEFAULT_CRYPTO_DECIMALS)
+									.toString()
+							);
 						}
 					}
 				}}
@@ -1048,8 +1075,10 @@ const useExchange = ({
 
 						if (txProvider === 'synthetix') {
 							const baseCurrencyAmountNoFee = baseCurrencyBalance.mul(inverseRate);
-							const fee = baseCurrencyAmountNoFee.mul(exchangeFeeRate ?? 1);
-							setQuoteCurrencyAmount(baseCurrencyAmountNoFee.add(fee).toString());
+							const fee = baseCurrencyAmountNoFee.mul(exchangeFeeRate ?? 0);
+							setQuoteCurrencyAmount(
+								baseCurrencyAmountNoFee.add(fee).toFixed(DEFAULT_CRYPTO_DECIMALS).toString()
+							);
 						}
 					}
 				}}
@@ -1071,7 +1100,7 @@ const useExchange = ({
 				<SelectCurrencyModal
 					onDismiss={() => setSelectBaseCurrencyModalOpen(false)}
 					onSelect={(currencyKey) => {
-						setBaseCurrencyAmount('');
+						setQuoteCurrencyAmount('');
 						// @ts-ignore
 						setCurrencyPair((pair) => ({
 							base: currencyKey,
@@ -1092,7 +1121,7 @@ const useExchange = ({
 				<SelectCurrencyModal
 					onDismiss={() => setSelectBaseTokenModalOpen(false)}
 					onSelect={(currencyKey) => {
-						setBaseCurrencyAmount('');
+						setQuoteCurrencyAmount('');
 						// @ts-ignore
 						setCurrencyPair((pair) => ({
 							base: currencyKey,
