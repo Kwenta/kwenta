426b1db30fe4dd196bda916208f24f0e2410867e
Brian Rossetti <brossetti1@gmail.com>
2022-05-19 16:16:03 -0600

    refactoring fee info box

--- sections/futures/FeeInfoBox/FeeInfoBox.tsx
+++ sections/futures/FeeInfoBox/FeeInfoBox.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
-import Wei from '@synthetixio/wei';
+import Wei, { WeiSource } from '@synthetixio/wei';
 
 import InfoBox from 'components/InfoBox';
 import useSelectedPriceCurrency from 'hooks/useSelectedPriceCurrency';
 import { formatCurrency, formatPercent, zeroBN } from 'utils/formatters/number';
 import { NO_VALUE } from 'constants/placeholder';
 import useGetNextPriceDetails from 'queries/futures/useGetNextPriceDetails';
-import { computeMarketFee, computeNPFee } from 'utils/costCalculations';
+
 import Connector from 'containers/Connector';
 import { getMarketKey } from 'utils/futures';
 import { useTranslation } from 'react-i18next';
 
 import TimerIcon from 'assets/svg/app/timer.svg';
 import StyledTooltip from 'components/Tooltip/StyledTooltip';
+import { computeNPFee, computeMarketFee } from 'utils/costCalculations';
 
 type FeeInfoBoxProps = {
 	currencyKey: string | null;
 	orderType: number;
 	feeCost: Wei | null;
-	dynamicFee: Wei | null;
+	dynamicFee: Wei | WeiSource | null;
 	sizeDelta: Wei;
 };
 
@@ -65,75 +66,84 @@ const FeeInfoBox: React.FC<FeeInfoBoxProps> = ({
 		</StyledTooltip>
 	);
 
-	return orderType === 1 ? (
+	const marketCostTooltip = (
+		<>
+			{formatPercent(staticRate ?? zeroBN)}
+			{dynamicFee?.gt(0) && (
+				<>
+					+<StyledDynamicFee className="yellow">{formatPercent(dynamicFee)}</StyledDynamicFee>
+					{tooltip()}
+				</>
+			)}
+		</>
+	);
+
+	return (
 		<StyledInfoBox
 			details={{
-				'Keeper Deposit': {
-					value: !!costDetails?.keeperDeposit
-						? formatCurrency(selectedPriceCurrency.name, costDetails.keeperDeposit, {
-								sign: selectedPriceCurrency.sign,
-								minDecimals: 2,
-						  })
-						: NO_VALUE,
-				},
-				'Commit Deposit': {
-					value: !!commitDeposit
-						? formatCurrency(selectedPriceCurrency.name, commitDeposit, {
-								sign: selectedPriceCurrency.sign,
-								minDecimals: commitDeposit.lt(0.01) ? 4 : 2,
-						  })
-						: NO_VALUE,
-				},
-				'Total Deposit': {
-					value: formatCurrency(selectedPriceCurrency.name, totalDeposit, {
-						sign: selectedPriceCurrency.sign,
-						minDecimals: 2,
-					}),
-					spaceBeneath: true,
-				},
-				'Next-Price Discount': {
-					value: !!nextPriceDiscount
-						? formatCurrency(selectedPriceCurrency.name, nextPriceDiscount, {
-								sign: selectedPriceCurrency.sign,
-								minDecimals: 2,
-						  })
-						: NO_VALUE,
-					color: nextPriceDiscount.lt(0) ? 'green' : nextPriceDiscount.gt(0) ? 'red' : undefined,
-				},
-				'Estimated Fees': {
-					value: formatCurrency(
-						selectedPriceCurrency.name,
-						totalDeposit.add(nextPriceDiscount ?? zeroBN),
-						{
-							minDecimals: 2,
-							sign: selectedPriceCurrency.sign,
-						}
-					),
-					tooltip: dynamicFee?.gt(0) ? tooltip() : null,
-				},
+				...(orderType === 1
+					? {
+							'Keeper Deposit': {
+								value: !!costDetails?.keeperDeposit
+									? formatCurrency(selectedPriceCurrency.name, costDetails.keeperDeposit, {
+											sign: selectedPriceCurrency.sign,
+											minDecimals: 2,
+									  })
+									: NO_VALUE,
+							},
+							'Commit Deposit': {
+								value: !!commitDeposit
+									? formatCurrency(selectedPriceCurrency.name, commitDeposit, {
+											sign: selectedPriceCurrency.sign,
+											minDecimals: commitDeposit.lt(0.01) ? 4 : 2,
+									  })
+									: NO_VALUE,
+							},
+							'Total Deposit': {
+								value: formatCurrency(selectedPriceCurrency.name, totalDeposit, {
+									sign: selectedPriceCurrency.sign,
+									minDecimals: 2,
+								}),
+								spaceBeneath: true,
+							},
+							'Next-Price Discount': {
+								value: !!nextPriceDiscount
+									? formatCurrency(selectedPriceCurrency.name, nextPriceDiscount, {
+											sign: selectedPriceCurrency.sign,
+											minDecimals: 2,
+									  })
+									: NO_VALUE,
+								color: nextPriceDiscount.lt(0)
+									? 'green'
+									: nextPriceDiscount.gt(0)
+									? 'red'
+									: undefined,
+							},
+							'Estimated Fees': {
+								value: formatCurrency(
+									selectedPriceCurrency.name,
+									totalDeposit.add(nextPriceDiscount ?? zeroBN),
+									{
+										minDecimals: 2,
+										sign: selectedPriceCurrency.sign,
+									}
+								),
+								tooltip: dynamicFee?.gt(0) ? tooltip() : null,
+							},
+					  }
+					: {
+							Fee: {
+								value: !!feeCost
+									? formatCurrency(selectedPriceCurrency.name, feeCost, {
+											sign: selectedPriceCurrency.sign,
+											minDecimals: feeCost.lt(0.01) ? 4 : 2,
+									  })
+									: NO_VALUE,
+								tooltip: marketCostTooltip,
+							},
+					  }),
 			}}
 		/>
-	) : (
-		<StyledContainer>
-			<div className="key">
-				Fee: {formatPercent(staticRate ?? zeroBN)}
-				{dynamicFee?.gt(0) && (
-					<>
-						{'+'}
-						<span className="yellow">{formatPercent(dynamicFee)}</span>
-						{tooltip()}
-					</>
-				)}
-			</div>
-			<div className="value">
-				{!!feeCost
-					? formatCurrency(selectedPriceCurrency.name, feeCost, {
-							sign: selectedPriceCurrency.sign,
-							minDecimals: feeCost.lt(0.01) ? 4 : 2,
-					  })
-					: NO_VALUE}
-			</div>
-		</StyledContainer>
 	);
 };
 
@@ -141,30 +151,8 @@ const StyledInfoBox = styled(InfoBox)`
 	margin-bottom: 16px;
 `;
 
-const StyledContainer = styled.div`
-	border: ${(props) => props.theme.colors.selectedTheme.border};
-	border-radius: 10px;
-	padding: 14px;
-	box-sizing: border-box;
-	width: 100%;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-
-	.key {
-		color: ${(props) => props.theme.colors.selectedTheme.input.placeholder};
-		font-size: 12px;
-	}
-
-	.value {
-		color: ${(props) => props.theme.colors.common.primaryWhite};
-		font-family: ${(props) => props.theme.fonts.mono};
-		font-size: 12px;
-	}
-
-	.yellow {
-		color: ${(props) => props.theme.colors.yellow};
-	}
+const StyledDynamicFee = styled.span`
+	color: ${(props) => props.theme.colors.yellow};
 `;
 
 const StyledTimerIcon = styled(TimerIcon)`
