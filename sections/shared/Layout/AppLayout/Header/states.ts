d5e3c5fccd6bb3d9776d191a8357166828ef4521
Yadong Zhang <zhyd007@gmail.com>
2022-06-01 02:22:07 +0000

    [1/5] hide Futures & Leaderboard in nav bar.

--- sections/shared/Layout/AppLayout/Header/states.ts
+++ sections/shared/Layout/AppLayout/Header/states.ts
@@ -1,9 +1,18 @@
+import ROUTES from 'constants/routes';
 import { selector } from 'recoil';
+import { isL2KovanState, isL2MainnetState } from 'store/wallet';
 import { MENU_LINKS, MenuLink } from './constants';
 
 export const menuLinksState = selector<MenuLink[]>({
 	key: 'menuLinksState',
-	get: () => {
-		return MENU_LINKS.filter((menuLink: MenuLink) => menuLink.link);
+	get: ({ get }) => {
+		const isL2Mainnet = get(isL2MainnetState);
+		const isL2Kovan = get(isL2KovanState);
+
+		return MENU_LINKS.filter((menuLink: MenuLink) =>
+			isL2Mainnet && isL2Kovan
+				? menuLink.link
+				: menuLink.link !== ROUTES.Markets.Home && menuLink.link !== ROUTES.Leaderboard.Home
+		);
 	},
 });
