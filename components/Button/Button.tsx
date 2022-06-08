650c119b1d6ce659410972e8246ad78e7638d93a
Leifu Chen <leifu.chen@gmail.com>
2022-06-08 15:45:59 +0300

    Fix the primary button background color

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -61,6 +61,12 @@ const Button = styled.button<ButtonProps>`
 	}
 
 	${(props) =>
+		!props.noOutline &&
+		css`
+			${border}
+		`};
+
+	${(props) =>
 		props.mono
 			? css`
 					font-family: ${props.theme.fonts.mono};
@@ -73,6 +79,7 @@ const Button = styled.button<ButtonProps>`
 		props.variant === 'primary' &&
 		css`
 			background: ${props.theme.colors.selectedTheme.button.primary.background};
+			text-shadow: ${props.theme.colors.selectedTheme.button.primary.textShadow};
 			&:hover {
 				background: ${props.theme.colors.selectedTheme.button.primary.hover};
 			}
@@ -103,12 +110,6 @@ const Button = styled.button<ButtonProps>`
 		css`
 			height: 50px;
 			min-width: 200px;
-		`};
-
-	${(props) =>
-		!props.noOutline &&
-		css`
-			${border}
 		`};
 
 	${(props) =>
