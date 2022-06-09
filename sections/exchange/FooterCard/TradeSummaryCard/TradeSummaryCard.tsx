6fd87d9d5e432cd00cb1b21615a813efe1047377
Ruku <55772762+lohba@users.noreply.github.com>
2022-06-09 12:53:16 -0400

    feat: Show Network Fees in USD on L2 (#952)
    * feat: show USD value of Gas Costs on L2
    
    * fixes
    
    * add justify-content to SummaryItems
    
    * fix lint warnings
    
    * minor layout fixes
    
    * update null value for network gas fee
    
    * edits
    
    Co-authored-by: lohba <lohba@lohbas-MacBook-Pro.local>
    Co-authored-by: platschi <platschi@posteo.org>

--- sections/exchange/FooterCard/TradeSummaryCard/TradeSummaryCard.tsx
+++ sections/exchange/FooterCard/TradeSummaryCard/TradeSummaryCard.tsx
@@ -22,8 +22,6 @@ import { SummaryItems, SummaryItem, SummaryItemLabel, SummaryItemValue } from '.
 import GasPriceSelect from 'sections/shared/components/GasPriceSelect';
 import FeeRateSummaryItem from 'sections/shared/components/FeeRateSummary';
 import FeeCostSummaryItem from 'sections/shared/components/FeeCostSummary';
-
-import TotalTradePriceSummaryItem from './TotalTradePriceSummaryItem';
 import { GasPrices } from '@synthetixio/queries';
 import PoweredBy1Inch from 'components/PoweredBy1Inch';
 import { Synth } from '@synthetixio/contracts-interface';
@@ -82,18 +80,16 @@ const TradeSummaryCard: FC<TradeSummaryCardProps> = ({
 	const summaryItems = (
 		<SummaryItems attached={attached}>
 			<GasPriceSelect gasPrices={gasPrices} transactionFee={transactionFee} />
-			<SummaryItem>
-				{isCreateShort ? (
+			{isCreateShort && (
+				<SummaryItem>
 					<>
 						<SummaryItemLabel>{t('shorting.common.interestRate')}</SummaryItemLabel>
 						<SummaryItemValue data-testid="short-interest-rate">
 							{formatPercent((shortInterestRate ?? 0).toString())}
 						</SummaryItemValue>
 					</>
-				) : (
-					<TotalTradePriceSummaryItem totalTradePrice={totalTradePrice} />
-				)}
-			</SummaryItem>
+				</SummaryItem>
+			)}
 			{showFee && (
 				<>
 					<FeeRateSummaryItem totalFeeRate={totalFeeRate} baseFeeRate={baseFeeRate} />
