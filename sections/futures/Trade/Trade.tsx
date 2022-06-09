56036892349129c135f21365c92d9949bd6db892
Troy <troy.burmeister@gmail.com>
2022-06-09 13:27:28 -0600

    fix error

--- sections/futures/Trade/Trade.tsx
+++ sections/futures/Trade/Trade.tsx
@@ -161,7 +161,7 @@ const Trade: React.FC<TradeProps> = ({
 			const size = fromLeverage ? (value === '' ? '' : wei(value).toNumber().toString()) : value;
 			const sizeSUSD = value === '' ? '' : marketAssetRate.mul(Number(value)).toNumber().toString();
 			const leverage =
-				value === ''
+				value === '' || !position?.remainingMargin
 					? ''
 					: marketAssetRate
 							.mul(Number(value))
