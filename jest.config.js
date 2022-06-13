4c1d424c13ea709694519c4832e1a1ec3596ebb2
Xiaolei-Shawn <xiaolei.shawn@gmail.com>
2022-06-13 19:59:25 +0300

    Remove useless comments in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
-// jest.config.js
 const nextJest = require('next/jest');
 
-// Providing the path to your Next.js app which will enable loading next.config.js and .env files
 const createJestConfig = nextJest({ dir: './' });
 
-// Any custom config you want to pass to Jest
 const customJestConfig = {
 	roots: ['<rootDir>'],
 	modulePaths: ['<rootDir>'],
@@ -14,5 +11,4 @@ const customJestConfig = {
 	testEnvironment: 'jest-environment-jsdom',
 };
 
-// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
 module.exports = createJestConfig(customJestConfig);
