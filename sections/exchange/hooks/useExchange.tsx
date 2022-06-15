6a1f2c6ecf9e5e21dc295d6d92950a979b1ea44a
platschi <platschi@posteo.org>
2022-06-15 08:04:37 -0300

    fix type error

--- sections/exchange/hooks/useExchange.tsx
+++ sections/exchange/hooks/useExchange.tsx
@@ -1202,7 +1202,6 @@ const useExchange = ({
 					// show fee's only for "synthetix" (provider)
 					showFee={txProvider === 'synthetix' ? true : false}
 					isApproved={needsApproval ? isApproved : undefined}
-					show1InchProvider={txProvider === '1inch'}
 				/>
 			)}
 			{txConfirmationModalOpen && (
