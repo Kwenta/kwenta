ab6c6f5ad47fe2c0487c76ac4d8eeb0bab78208c
Troy <troy.burmeister@gmail.com>
2022-07-08 10:42:12 -0600

    fix table no results

--- sections/dashboard/FuturesHistoryTable/FuturesHistoryTable.tsx
+++ sections/dashboard/FuturesHistoryTable/FuturesHistoryTable.tsx
@@ -62,6 +62,7 @@ const FuturesHistoryTable: FC = () => {
 			<StyledTable
 				data={isL2 ? mappedHistoricalTrades : []}
 				showPagination={true}
+				isLoading={futuresTradesQuery.isLoading || futuresTradesQuery.isIdle}
 				noResultsMessage={
 					!isL2 ? (
 						<TableNoResults>
