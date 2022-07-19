330f86a322dabb4df42406423e941f0248bd7772
Troy <troy.burmeister@gmail.com>
2022-07-19 17:00:02 -0600

    fix titles

--- pages/leaderboard/[[...tab]].tsx
+++ pages/leaderboard/[[...tab]].tsx
@@ -19,7 +19,7 @@ const Leader: LeaderComponent = () => {
 	return (
 		<>
 			<Head>
-				<title>{t('futures.page-title')}</title>
+				<title>{t('leaderboard.page-title')}</title>
 			</Head>
 			<PageContent>
 				<MobileHiddenView>
