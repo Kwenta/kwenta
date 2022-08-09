ed14ededc21f2aa9ccf4a80aee53a1cc743a8739
Yadong Zhang <zhyd007@gmail.com>
2022-08-09 13:24:58 +0800

    chore: added necessary ts doc.

--- hooks/useDashboardCompetition.ts
+++ hooks/useDashboardCompetition.ts
@@ -6,6 +6,26 @@ import { calculatedTimeDifference } from 'utils/formatters/date';
 
 type TCompetitionSate = 'comingSoon' | 'live' | 'comingToEnd' | 'ended';
 
+/**
+ * @example
+ * ```
+ * const START_DATE = new Date(2022, 7, 18);
+ * const END_DATE = new Date(2022, 7, 22);
+ *
+ * const diff_1 = differenceInSeconds(START_DATE, new Date(2022, 7, 9));
+ * console.log('coming soon: ', diff_1); // 777600
+ *
+ * const diff_2_1 = differenceInSeconds(START_DATE, new Date(2022, 7, 19));
+ * const diff_2_2 = differenceInSeconds(END_DATE, new Date(2022, 7, 19));
+ * console.log('live: ', diff_2_1, diff_2_2); // -86400, 259200
+ *
+ * const diff_3 = differenceInSeconds(END_DATE, new Date(2022, 7, 21, 1));
+ * console.log('will be ended in 24 hours: ', diff_3); // 0 < 82800 < 60 * 60 * 24
+ *
+ * const diff_4 = differenceInSeconds(END_DATE, new Date(2022, 7, 23));
+ * console.log('ended: ', diff_4); // -86400
+ * ```
+ */
 export const useDashboardCompetition = () => {
 	const [state, setState] = useState<TCompetitionSate>('comingSoon');
 	const [difference, setDifference] = useState<number>(0);
