be9386caf2500b29795f8c0991c14bc8d7418982
Yadong Zhang <zhyd007@gmail.com>
2022-08-10 13:11:34 +0800

    feat: mobile stats page title.

--- sections/stats/Stats.tsx
+++ sections/stats/Stats.tsx
@@ -1,14 +1,34 @@
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+	@media only screen and (min-width: 600px) {
+		// TODO: add media query for desktop
+	}
+	@media only screen and (min-width: 768px) {
+		// TODO: add media query for tablet
+	}
+	@media only screen and (min-width: 992px) {
+		// TODO: add media query for desktop
+	}
+	@media only screen and (min-width: 1200px) {
+		// TODO: add media query for desktop
+	}
+`;
+
+const StatsTitle = styled.h3`
+	font-family: ${(props) => props.theme.fonts.compressedBlack};
+	font-size: 24px;
+	color: ${(props) => props.theme.colors.common.primaryWhite};
+	text-transform: uppercase;
+`;
 
 export const Stats = () => {
 	const { t } = useTranslation();
 
 	return (
 		<Container>
-			<h3>{t('stats.title')}</h3>
+			<StatsTitle>{t('stats.title')}</StatsTitle>
 		</Container>
 	);
 };
