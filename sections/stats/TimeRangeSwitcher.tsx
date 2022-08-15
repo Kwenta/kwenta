7cb02d215ddf8fd6de9a135851a5efcbb754bd7b
Yadong Zhang <zhyd007@gmail.com>
2022-08-15 19:50:24 +0800

    feat: init TimeRangeSwitcher component.

---
+++ sections/stats/TimeRangeSwitcher.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+import Button from 'components/Button';
+
+const Container = styled.div`
+	display: flex;
+	gap: 4px;
+`;
+
+const StyledBtn = styled(Button)`
+	width: 40px;
+	height: 24px;
+	border: 0.669444px solid rgba(255, 255, 255, 0.1);
+	border-radius: 7px;
+	box-shadow: 0px 1.33889px 1.33889px rgba(0, 0, 0, 0.25),
+		inset 0px 0px 13.3889px rgba(255, 255, 255, 0.03);
+	font-family: ${(props) => props.theme.fonts.bold};
+	font-size: 11px;
+	text-align: center;
+	text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.5);
+	padding: 0;
+
+	&:hover,
+	&:active {
+		border-color: ${(props) => props.theme.colors.common.primaryGold};
+	}
+`;
+
+export const TimeRangeSwitcher = () => {
+	return (
+		<Container>
+			{['24H', '1W', '1M', 'MAX'].map((range: string) => (
+				<StyledBtn key={range}>{range}</StyledBtn>
+			))}
+		</Container>
+	);
+};
