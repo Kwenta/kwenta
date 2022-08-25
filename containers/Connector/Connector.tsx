ab292cff04dff78bc6044b2d5bf927dc455413c7
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-08-25 15:57:14 -0400

    Put defaultSynthetixjs in memo

--- containers/Connector/Connector.tsx
+++ containers/Connector/Connector.tsx
@@ -5,23 +5,22 @@ import { useMemo } from 'react';
 import { createContainer } from 'unstated-next';
 import { chain, useNetwork, useProvider } from 'wagmi';
 
-import { DEFAULT_NETWORK_ID } from 'constants/defaults';
-
 const useConnector = () => {
 	const { chain: activeChain } = useNetwork();
 	const network =
 		activeChain !== undefined && activeChain.unsupported
 			? chain.optimism
 			: activeChain ?? chain.optimism;
-	const provider = useProvider({
-		chainId: network.id,
-	});
+
+	const provider = useProvider({ chainId: network.id });
+
 	// Provides a default mainnet provider, irrespective of the current network
 	const staticMainnetProvider = new ethers.providers.InfuraProvider();
-	const defaultSynthetixjs = synthetix({
-		provider: provider,
-		networkId: network.id as NetworkId,
-	});
+
+	const defaultSynthetixjs = useMemo(
+		() => synthetix({ provider, networkId: network.id as NetworkId }),
+		[provider, network.id]
+	);
 
 	const [synthsMap, tokensMap] = useMemo(() => {
 		if (defaultSynthetixjs == null) return [{}, {}];
@@ -29,12 +28,7 @@ const useConnector = () => {
 		return [keyBy(defaultSynthetixjs.synths, 'name'), keyBy(defaultSynthetixjs.tokens, 'symbol')];
 	}, [defaultSynthetixjs]);
 
-	return {
-		synthsMap,
-		tokensMap,
-		staticMainnetProvider,
-		defaultSynthetixjs,
-	};
+	return { synthsMap, tokensMap, staticMainnetProvider, defaultSynthetixjs };
 };
 
 const Connector = createContainer(useConnector);
