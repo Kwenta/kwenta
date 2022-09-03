ad2bbe347eaa04c9924b9658a97be103fff7b9ee
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-09-02 22:32:02 -0400

    Restructure components to help re-render perf

--- sections/futures/LeftSidebar/LeftSidebar.tsx
+++ sections/futures/LeftSidebar/LeftSidebar.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { LeftSideContent } from 'styles/common';
+
 import FuturesMarketTabs from '../FuturesMarketTabs';
 import SkewInfo from '../TradingHistory/SkewInfo';
 
-const TradingHistory: React.FC = React.memo(() => {
-	return (
+const TradingHistory: React.FC = React.memo(() => (
+	<StyledLeftSideContent>
 		<Panel>
 			<SkewInfo />
 			<FuturesMarketTabs />
 		</Panel>
-	);
-});
+	</StyledLeftSideContent>
+));
 
 export default TradingHistory;
+
+const StyledLeftSideContent = styled(LeftSideContent)`
+	width: 100%;
+
+	@media (max-width: 1200px) {
+		display: none;
+	}
+`;
 
 const Panel = styled.div`
 	padding-bottom: 20px;
