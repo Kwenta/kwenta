7c72cb7cce0276bcfa2fde2c4452d6df155be113
leifu <leifu.chen@gmail.com>
2022-09-06 00:11:19 +0300

    hotfix: ETH swap token address (#1355)
    * Disabled no synth card
    * Added ETH approval
    * Limit the liquidity source
    * Update the token address
    * Fixed the lint error and update the token address
    * Fix the swap pricing display

--- queries/coingecko/useCoinGeckoTokenPricesQuery.ts
+++ queries/coingecko/useCoinGeckoTokenPricesQuery.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { UseQueryOptions, useQuery } from 'react-query';
 
+import { ETH_ADDRESS, ETH_COINGECKO_ADDRESS } from 'constants/currency';
 import QUERY_KEYS from 'constants/queryKeys';
 import useIsL2 from 'hooks/useIsL2';
 
@@ -18,9 +19,9 @@ const useCoinGeckoTokenPricesQuery = (
 		QUERY_KEYS.CoinGecko.TokenPrices(tokenAddresses, platform),
 		async () => {
 			const response = await axios.get<PriceResponse>(
-				`${CG_BASE_API_URL}/simple/token_price/${platform}?contract_addresses=${tokenAddresses.join(
-					','
-				)}&vs_currencies=usd`
+				`${CG_BASE_API_URL}/simple/token_price/${platform}?contract_addresses=${tokenAddresses
+					.join(',')
+					.replace(ETH_ADDRESS, ETH_COINGECKO_ADDRESS)}&vs_currencies=usd`
 			);
 			return response.data;
 		},
