49520eccc92628d87e729eb249dc66606b878ca6
Leifu Chen <leifu.chen@gmail.com>
2022-09-06 20:45:36 +0300

    Update the synth endpoint and clean up kovan

--- queries/synths/utils.ts
+++ queries/synths/utils.ts
@@ -1,11 +1,4 @@
-import { NetworkId } from '@synthetixio/contracts-interface';
 import { Contract } from 'ethcall';
-
-import {
-	SYNTHS_ENDPOINT_MAIN,
-	SYNTHS_ENDPOINT_OPTIMISM_KOVAN,
-	SYNTHS_ENDPOINT_OPTIMISM_MAIN,
-} from './constants';
 
 const abi = [
 	{
@@ -22,14 +15,6 @@ const abi = [
 export const getProxySynthSymbol = (address: string) => {
 	const c = new Contract(address, abi);
 	return c.symbol();
-};
-
-export const getSynthsEndpoint = (networkId: NetworkId): string => {
-	return networkId === 10
-		? SYNTHS_ENDPOINT_OPTIMISM_MAIN
-		: networkId === 69
-		? SYNTHS_ENDPOINT_OPTIMISM_KOVAN
-		: SYNTHS_ENDPOINT_MAIN;
 };
 
 export function notNill<Value>(value: Value | null | undefined): value is Value {
