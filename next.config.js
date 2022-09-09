1869daf38ca0fa45a1d52395bf951ea211436368
Yadong Zhang <zhyd007@gmail.com>
2022-09-09 20:32:27 +0800

    build: removed redundant eslint and ts check in next config file.

--- next.config.js
+++ next.config.js
@@ -99,17 +99,5 @@ module.exports = withPlugins([
 				},
 			];
 		},
-		// eslint: {
-		// 	// Warning: This allows production builds to successfully complete even if
-		// 	// your project has ESLint errors.
-		// 	ignoreDuringBuilds: true,
-		// },
-		// typescript: {
-		// 	// !! WARN !!
-		// 	// Dangerously allow production builds to successfully complete even if
-		// 	// your project has type errors.
-		// 	// !! WARN !!
-		// 	ignoreBuildErrors: true,
-		// },
 	},
 ]);
