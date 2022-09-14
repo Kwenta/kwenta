3c1afdec06bcefaa93ad34092c9dd23b84ab06e3
léo <28714795+leovct@users.noreply.github.com>
2022-09-14 15:11:18 +0200

    add spot markets to mobile (#1342)
    * implement the mobile version of SpotMarketsTable
    
    * create a new SpotMarkets page
    
    * allow switching between futures and spot markets
    
    * use translations instead of hardcoded strings in FuturesMarketsTable
    
    * track activePositionsTab state using recoil to share it between desktop and mobile versions
    
    * fix problem with 24h change (mobile version)
    
    * use translations instead of hardcoded strings
    
    * little fix on activePositionsTab state
    
    * fix typo on Futures Markets label
    
    Co-authored-by: platschi <platschi@posteo.org>

--- sections/dashboard/MobileDashboard/MobileDashboard.tsx
+++ sections/dashboard/MobileDashboard/MobileDashboard.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
 
 import { CompetitionBanner } from 'sections/shared/components/CompetitionBanner';
+import { activePositionsTabState } from 'store/ui';
 
+import { MarketsTab } from '../Markets/Markets';
 import FuturesMarkets from './FuturesMarkets';
 import OpenPositions from './OpenPositions';
 import Portfolio from './Portfolio';
+import SpotMarkets from './SpotMarkets';
 
-const MobileDashboard: React.FC = () => (
-	<div>
-		<CompetitionBanner />
-		<Portfolio />
-		<OpenPositions />
-		<FuturesMarkets />
-	</div>
-);
+const MobileDashboard: React.FC = () => {
+	// in the mobile dashboard, there are no differences between positions and markets tab
+	const [activePositionsTab, setActivePositionsTab] = useRecoilState(activePositionsTabState);
+
+	return (
+		<div>
+			<CompetitionBanner />
+			<Portfolio />
+			<OpenPositions
+				activePositionsTab={activePositionsTab}
+				setActivePositionsTab={setActivePositionsTab}
+			/>
+			{activePositionsTab === MarketsTab.FUTURES && <FuturesMarkets />}
+			{activePositionsTab === MarketsTab.SPOT && <SpotMarkets />}
+		</div>
+	);
+};
 
 export default MobileDashboard;
--- sections/dashboard/MobileDashboard/Portfolio.tsx
+++ sections/dashboard/MobileDashboard/Portfolio.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { SectionHeader, SectionTitle } from 'sections/futures/MobileTrade/common';
 
 import PortfolioChart from '../PortfolioChart';
 
-const Portfolio: React.FC = () => (
-	<div>
-		<div style={{ margin: 15 }}>
-			<SectionHeader>
-				<SectionTitle>Portfolio</SectionTitle>
-			</SectionHeader>
+const Portfolio: React.FC = () => {
+	const { t } = useTranslation();
+
+	return (
+		<div>
+			<div style={{ margin: 15 }}>
+				<SectionHeader>
+					<SectionTitle>{t('dashboard.overview.mobile.portfolio')}</SectionTitle>
+				</SectionHeader>
+			</div>
+			<PortfolioChart />
 		</div>
-		<PortfolioChart />
-	</div>
-);
+	);
+};
 
 export default Portfolio;
