c0e4c90f49d0a72814c85cc99217f8ddb19a7690
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-10-07 11:07:17 -0400

    Remove SDKContext

--- contexts/SDKContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-import { sdk } from 'state/store';
-
-export const SDKContext = createContext({
-	setProvider: sdk.setProvider,
-	setNetworkId: sdk.setNetworkId,
-});
-
-export const useSDKContext = () => useContext(SDKContext);
