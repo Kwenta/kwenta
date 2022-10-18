489cd5b8a0a1a5483bacfa8346ab7a8adb08005b
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-10-17 22:16:16 -0400

    Delete most of exchange recoil state

--- sections/exchange/FooterCard/MarketClosureCard/MarketClosureCard.tsx
+++ sections/exchange/FooterCard/MarketClosureCard/MarketClosureCard.tsx
@@ -1,21 +1,21 @@
 import { FC, useMemo } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { useRecoilValue } from 'recoil';
+import { useAppSelector } from 'state/store';
 
 import { DesktopOnlyView, MobileOrTabletView } from 'components/Media';
 import useMarketClosed from 'hooks/useMarketClosed';
-import { baseCurrencyKeyState, quoteCurrencyKeyState } from 'store/exchange';
 
 import { MessageContainer, Message, MessageButton, FixedMessageContainerSpacer } from '../common';
 
 const MarketClosureCard: FC = () => {
 	const { t } = useTranslation();
+	const { quoteCurrencyKey, baseCurrencyKey } = useAppSelector(({ exchange }) => ({
+		quoteCurrencyKey: exchange.quoteCurrencyKey,
+		baseCurrencyKey: exchange.baseCurrencyKey,
+	}));
 
-	const baseCurrencyKey = useRecoilValue(baseCurrencyKeyState);
-	const quoteCurrencyKey = useRecoilValue(quoteCurrencyKeyState);
-
-	const quoteCurrencyMarketClosed = useMarketClosed(quoteCurrencyKey);
-	const baseCurrencyMarketClosed = useMarketClosed(baseCurrencyKey);
+	const quoteCurrencyMarketClosed = useMarketClosed(quoteCurrencyKey ?? null);
+	const baseCurrencyMarketClosed = useMarketClosed(baseCurrencyKey ?? null);
 
 	const getSuspensionReason = useMemo(() => {
 		if (baseCurrencyMarketClosed.isMarketClosed && quoteCurrencyMarketClosed.isMarketClosed) {
