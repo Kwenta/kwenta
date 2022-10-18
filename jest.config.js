5d7b23a9f5337f60f0b86c9290f654ac6d7d2a8b
Adam Clarke <adam@adamclarke.co>
2022-10-18 20:05:14 +0100

    Unit test mocks and initial cross margin tests (#1482)
    * Unit test setup and initial cross margin tests
    
    * Cleanup
    
    * Fix package lock
    
    * Move page tests and update README
    
    * fix lint warning
    
    Co-authored-by: platschi <platschi@posteo.org>

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,20 @@ const customJestConfig = {
 	roots: ['<rootDir>'],
 	modulePaths: ['<rootDir>'],
 	moduleDirectories: ['node_modules'],
-	globalSetup: './test-setup/global.js',
-	setupFilesAfterEnv: ['./test-setup/setup.js'],
+	globalSetup: './testing/unit/setup/global.js',
+	setupFilesAfterEnv: ['./testing/unit/setup/setup.js'],
 	testEnvironment: 'jest-environment-jsdom',
+	transform: {
+		'^.+\\.(svg)$': `jest-transformer-svg`,
+	},
 };
 
-module.exports = createJestConfig(customJestConfig);
+const getCustomConfig = async () => {
+	// Delete next js module name mapper transform and use above svg
+	// transformer to avoid errors with svg and styled components
+	const config = await createJestConfig(customJestConfig)();
+	delete config['moduleNameMapper']['^.+\\.(svg)$'];
+	return config;
+};
+
+module.exports = getCustomConfig();
--- test-setup/setup.js
+++
@@ -1,4 +0,0 @@
-import '@testing-library/jest-dom';
-import { TextEncoder, TextDecoder } from 'util';
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
