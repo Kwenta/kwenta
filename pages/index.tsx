93dd85f8d436edc51594ffe675c23c46f7b328e7
Troy <troy.burmeister@gmail.com>
2022-10-24 12:07:17 -0600

    simplify routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
@@ -12,8 +11,6 @@ import ShortList from 'sections/homepage/ShortList';
 import TradeNow from 'sections/homepage/TradeNow';
 import GitHashID from 'sections/shared/Layout/AppLayout/GitHashID';
 import HomeLayout from 'sections/shared/Layout/HomeLayout';
-import type { TPages } from 'sections/shared/Layout/HomeLayout/Header';
-import { Stats } from 'sections/stats';
 import media from 'styles/media';
 
 type AppLayoutProps = {
@@ -24,18 +21,6 @@ type HomePageComponent = FC & { layout?: FC<AppLayoutProps> };
 
 const HomePage: HomePageComponent = () => {
 	const { t } = useTranslation();
-	const router = useRouter();
-
-	const queryPage = router.query.page;
-	const [currentPage, setCurrentPage] = useState<TPages>('landing-page');
-
-	useEffect(() => {
-		if (queryPage === 'stats') {
-			setCurrentPage('stats-page');
-		} else {
-			setCurrentPage('landing-page');
-		}
-	}, [router, queryPage]);
 
 	const Assets = dynamic(() => import('../sections/homepage/Assets'), {
 		ssr: false,
@@ -47,19 +32,15 @@ const HomePage: HomePageComponent = () => {
 				<title>{t('homepage.page-title')}</title>
 			</Head>
 			<HomeLayout>
-				{currentPage === 'landing-page' ? (
-					<Container>
-						<Hero />
-						<Assets />
-						<ShortList />
-						<Earning />
-						<Features />
-						<TradeNow />
-						<GitHashID />
-					</Container>
-				) : (
-					<Stats />
-				)}
+				<Container>
+					<Hero />
+					<Assets />
+					<ShortList />
+					<Earning />
+					<Features />
+					<TradeNow />
+					<GitHashID />
+				</Container>
 			</HomeLayout>
 		</>
 	);
