17f8cc1751205f731597e950eceb24bd2826d2e7
0xwalde <0xwalde@proton.me>
2022-11-29 10:49:08 -0500

    Extracted Banner component from HomeLayout for easier management

---
+++ constants/announcement.ts
@@ -0,0 +1,9 @@
+// The announcement banner on the home page can be toggled on and off by setting
+// the `BANNER_TEXT` constant to a falsey value (`false`, `null`, '', etc...).
+// In addition, the URL that the banner text links out to can be set using the
+// `BANNER_LINK_URL` constant-- if this is falsey, the `BANNER_TEXT` will render
+// as non-interactive text instead of a link.
+export const BANNER_LINK_URL =
+	'https://snapshot.org/#/kwenta.eth/proposal/0x4a2dbd3839de2b6407ebbdc47e7d51a5d69f3363a803a93ffd8cf4e5d08011ff';
+export const BANNER_TEXT =
+	'Council elections are live on Snapshot until Nov 29th. $KWENTA stakers go cast your vote!';
---
+++ sections/shared/Layout/HomeLayout/Banner.tsx
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+
+import { DesktopOnlyView, MobileOrTabletView } from 'components/Media';
+import { BANNER_LINK_URL, BANNER_TEXT } from 'constants/announcement';
+import media from 'styles/media';
+
+const Banner = () => {
+	if (!BANNER_TEXT) return null;
+
+	const linkProps = BANNER_LINK_URL
+		? { href: BANNER_LINK_URL, target: '_blank' }
+		: { as: 'p' as const };
+	const bannerLink = <FuturesLink {...linkProps}>{BANNER_TEXT}</FuturesLink>;
+
+	return (
+		<>
+			<DesktopOnlyView>
+				<FuturesBannerContainer>
+					<FuturesBannerLinkWrapper>{bannerLink}</FuturesBannerLinkWrapper>
+				</FuturesBannerContainer>
+			</DesktopOnlyView>
+			<MobileOrTabletView>
+				<FuturesBannerContainer>{bannerLink}</FuturesBannerContainer>
+			</MobileOrTabletView>
+		</>
+	);
+};
+
+const FuturesLink = styled.a`
+	margin-right: 5px;
+	background: #313131;
+	padding: 4px 9px;
+	border-radius: 20px;
+`;
+
+const FuturesBannerContainer = styled.div`
+	height: 70px;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	margin-bottom: -35px;
+
+	${media.lessThan('md')`
+		position: relative;
+		width: 100%;
+		display: flex;
+		margin-bottom: 0px;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		background: transaparent;
+		padding: 22px 10px;
+		border-radius: 0px;
+		gap: 5px;
+	`}
+`;
+
+const FuturesBannerLinkWrapper = styled.div`
+	width: 100%;
+	text-align: center;
+	position: absolute;
+
+	color: ${(props) => props.theme.colors.white};
+	font-family: ${(props) => props.theme.fonts.bold};
+	font-size: 16px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+export default Banner;
