74c79b889d2622235f28fd3946265db2a4781eb7
0xwalde <109358247+0xwalde@users.noreply.github.com>
2022-11-29 16:16:19 -0500

    move banner into announcement component (#1695)
    * Extracted Banner component from HomeLayout for easier management
    
    * Add dedicated BANNER_ENABLED constant for toggling the home page banner
    
    * disable banner
    
    Co-authored-by: platschi <platschi@posteo.org>

---
+++ constants/announcement.ts
@@ -0,0 +1,9 @@
+// Shows or hides the home page banner entirely when set to true/false
+export const BANNER_ENABLED = false;
+// Sets the link destination for the banner component, or renders the banner as
+// plain, un-clickable text if set to a falsey value (`false`, `null`, '', etc...)
+export const BANNER_LINK_URL =
+	'https://snapshot.org/#/kwenta.eth/proposal/0x4a2dbd3839de2b6407ebbdc47e7d51a5d69f3363a803a93ffd8cf4e5d08011ff';
+// Sets the text displayed on the home page banner
+export const BANNER_TEXT =
+	'Council elections are live on Snapshot until Nov 29th. $KWENTA stakers go cast your vote!';
