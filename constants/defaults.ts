2a390ba9992c52c0d6b6b90963b82cc278bb9a62
Troy <troy.burmeister@gmail.com>
2022-12-12 14:09:57 -0700

    friendlier default

--- constants/defaults.ts
+++ constants/defaults.ts
@@ -3,6 +3,7 @@ import { Language } from 'translations/constants';
 
 import { languageStateKey, priceCurrencyStateKey } from 'store/app/constants';
 import localStore from 'utils/localStore';
+import { weiFromEth } from 'utils/formatters/number';
 
 // app defaults
 export const DEFAULT_LANGUAGE: Language = localStore.get(languageStateKey) ?? Language.EN;
@@ -48,7 +49,7 @@ export const DEFAULT_NP_LEVERAGE_ADJUSTMENT: number = 0.9975;
 export const DEFAULT_LEADERBOARD_ROWS = 20;
 
 // for perps v2
-export const DEFAULT_PRICE_IMPACT_DELTA = '500000000000000000';
+export const DEFAULT_PRICE_IMPACT_DELTA = weiFromEth(0.5).toString();
 export const DEFAULT_DELAYED_EXECUTION_BUFFER = 3;
 
 export const CROSS_MARGIN_ENABLED = false;
