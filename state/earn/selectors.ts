06466cb1385fe8091892e4d361907d120956cd77
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-12-13 17:08:08 -0500

    Use existing constant

--- state/earn/selectors.ts
+++ state/earn/selectors.ts
@@ -1,10 +1,9 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { wei } from '@synthetixio/wei';
 
+import { PERIOD_IN_SECONDS } from 'sdk/constants/period';
 import { RootState } from 'state/store';
 import { truncateNumbers } from 'utils/formatters/number';
-
-const SECONDS_PER_DAY = 86400;
 
 export const selectBalance = createSelector((state: RootState) => state.earn.balance, wei);
 
@@ -27,7 +26,7 @@ export const selectYieldPerDay = createSelector(
 	(state: RootState) => state.earn.totalSupply,
 	(balance, rewardRate, totalSupply) => {
 		const rawYield = wei(totalSupply).gt(0)
-			? wei(balance).mul(rewardRate).div(totalSupply).mul(SECONDS_PER_DAY)
+			? wei(balance).mul(rewardRate).div(totalSupply).mul(PERIOD_IN_SECONDS.ONE_DAY)
 			: wei(0);
 
 		return truncateNumbers(rawYield.toString(), 4);
