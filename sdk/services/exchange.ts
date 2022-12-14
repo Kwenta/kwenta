c52f2772c37977cbc03aa9292748e1a6fa5bf613
Leifu Chen <leifu.chen@gmail.com>
2022-12-14 15:07:09 +0200

    Updated the 1inch API from to v5

--- sdk/services/exchange.ts
+++ sdk/services/exchange.ts
@@ -889,7 +889,7 @@ export default class ExchangeService {
 	}
 
 	private get oneInchApiUrl() {
-		return `https://api.1inch.io/v4.0/${this.sdk.context.isL2 ? 10 : 1}/`;
+		return `https://api.1inch.io/v5.0/${this.sdk.context.isL2 ? 10 : 1}/`;
 	}
 
 	private getOneInchQuoteSwapParams(
