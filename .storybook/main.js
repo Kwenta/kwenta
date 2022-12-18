d0c482f4dd04384a25f1a6c1680547285275d853
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-12-18 06:26:07 -0500

    Fix SVGs in Storybook and add Headings

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,14 @@ module.exports = {
 	framework: '@storybook/react',
 	webpackFinal: async (config) => {
 		config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../')];
+		const fileLoaderRule = config.module.rules.find((rule) => rule.test && rule.test.test('.svg'));
+		fileLoaderRule.exclude = /\.svg$/;
+
+		config.module.rules.push({
+			test: /\.svg$/,
+			enforce: 'pre',
+			loader: require.resolve('@svgr/webpack'),
+		});
 
 		return config;
 	},
--- stories/Text.stories.tsx
+++ stories/Text.stories.tsx
@@ -6,9 +6,24 @@ export default {
 };
 
 export const BigText = () => {
-	return <Text.BigText>100</Text.BigText>;
+	return (
+		<div>
+			<Text.BigText>100</Text.BigText>
+			<Text.BigText yellow mono kwenta>
+				100
+			</Text.BigText>
+		</div>
+	);
 };
 
 export const Heading = () => {
-	return <div />;
+	return (
+		<div>
+			<Text.Heading variant="h1">Heading 1</Text.Heading>
+			<Text.Heading variant="h2">Heading 2</Text.Heading>
+			<Text.Heading variant="h3">Heading 3</Text.Heading>
+			<Text.Heading variant="h4">Heading 4</Text.Heading>
+			<Text.Heading variant="h5">Heading 5</Text.Heading>
+		</div>
+	);
 };
