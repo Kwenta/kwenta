bf72f3a74f22ecc786e437115b1707b834ac5556
Oluwakorede Fashokun <koredefashokun@gmail.com>
2022-12-22 02:51:34 -0500

    Remove unused Data component

--- components/Text/index.ts
+++ components/Text/index.ts
@@ -1,7 +1,6 @@
 import { default as BigText } from './BigText';
 import { default as Body } from './Body';
-import { default as Data } from './Data';
 import { default as Heading } from './Heading';
 import { default as PositionType } from './PositionType';
 
-export { Body, Data, Heading, BigText, PositionType };
+export { Body, Heading, BigText, PositionType };
