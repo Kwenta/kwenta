0833e992e0e15fc01816c835469993b546182c1c
Troy <troy.burmeister@gmail.com>
2023-01-03 13:49:41 -0700

    responsive title price

--- sections/futures/MarketInfo/MarketHead.tsx
+++ sections/futures/MarketInfo/MarketHead.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 
 import { DEFAULT_CRYPTO_DECIMALS } from 'constants/defaults';
 import useSelectedPriceCurrency from 'hooks/useSelectedPriceCurrency';
-import { selectMarketAsset, selectMarketPrice } from 'state/futures/selectors';
+import { selectMarketAsset, selectSkewAdjustedPrice } from 'state/futures/selectors';
 import { useAppSelector } from 'state/hooks';
 import { formatCurrency } from 'utils/formatters/number';
 import { getDisplayAsset, isDecimalFour } from 'utils/futures';
@@ -14,7 +14,7 @@ const MarketHead: React.FC = () => {
 	const { selectedPriceCurrency } = useSelectedPriceCurrency();
 
 	const marketAsset = useAppSelector(selectMarketAsset);
-	const latestPrice = useAppSelector(selectMarketPrice);
+	const latestPrice = useAppSelector(selectSkewAdjustedPrice);
 	const marketName = getDisplayAsset(marketAsset);
 
 	return (
