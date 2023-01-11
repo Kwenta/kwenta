144b1bdc49fe4ebbae4449b75749d41c52c21d72
Troy <troy.burmeister@gmail.com>
2023-01-11 12:23:19 -0700

    testnet subgraph endpoint

--- queries/rates/constants.ts
+++ queries/rates/constants.ts
@@ -6,7 +6,7 @@ export const RATES_ENDPOINT_OP_MAINNET =
 	'https://api.thegraph.com/subgraphs/name/kwenta/optimism-latest-rates';
 
 export const RATES_ENDPOINT_OP_GOERLI =
-	'https://api.thegraph.com/subgraphs/name/kwenta/optimism-goerli-main';
+	'https://api.thegraph.com/subgraphs/name/kwenta/optimism-goerli-latest-rates';
 
 export const RATES_ENDPOINT_GOERLI = 'https://api.thegraph.com/subgraphs/name/kwenta/goerli-main';
 
