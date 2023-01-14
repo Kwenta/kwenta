9541f9986b91296acbe6ded437e54b08dcc3e3b4
Oluwakorede Fashokun <koredefashokun@gmail.com>
2023-01-14 13:03:27 -0500

    MarketClosureIcon memo

--- components/MarketClosureIcon/MarketClosureIcon.tsx
+++ components/MarketClosureIcon/MarketClosureIcon.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import { FC, memo } from 'react';
 
 import CircuitBreakerIcon from 'assets/svg/app/market-closure/circuit-breaker.svg';
 import EmergencyShutdownIcon from 'assets/svg/app/market-closure/emergency-shutdown.svg';
@@ -11,27 +11,26 @@ type MarketClosureIconProps = {
 	size?: 'sm' | 'lg';
 };
 
-export const MarketClosureIcon: FC<MarketClosureIconProps> = ({
-	marketClosureReason,
-	size = 'lg',
-}) => {
-	const sharedProps = {
-		width: size === 'sm' ? 16 : 32,
-		height: size === 'sm' ? 16 : 32,
-		className: 'market-closure-icon',
-	};
+export const MarketClosureIcon: FC<MarketClosureIconProps> = memo(
+	({ marketClosureReason, size = 'lg' }) => {
+		const sharedProps = {
+			width: size === 'sm' ? 16 : 32,
+			height: size === 'sm' ? 16 : 32,
+			className: 'market-closure-icon',
+		};
 
-	switch (marketClosureReason) {
-		case 'frozen':
-			return <FrozenIcon {...sharedProps} />;
-		case 'circuit-breaker':
-			return <CircuitBreakerIcon {...sharedProps} />;
-		case 'emergency':
-			return <EmergencyShutdownIcon {...sharedProps} />;
-		case 'market-closure':
-		default:
-			return <MarketPauseIcon {...sharedProps} />;
+		switch (marketClosureReason) {
+			case 'frozen':
+				return <FrozenIcon {...sharedProps} />;
+			case 'circuit-breaker':
+				return <CircuitBreakerIcon {...sharedProps} />;
+			case 'emergency':
+				return <EmergencyShutdownIcon {...sharedProps} />;
+			case 'market-closure':
+			default:
+				return <MarketPauseIcon {...sharedProps} />;
+		}
 	}
-};
+);
 
 export default MarketClosureIcon;
