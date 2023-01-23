8256c5fa8d0f23715b7e31670882d4f047c65bac
Oluwakorede Fashokun <koredefashokun@gmail.com>
2023-01-23 06:41:48 -0500

    Add yellow color

--- sections/dashboard/Stake/TradingRewardsTab.tsx
+++ sections/dashboard/Stake/TradingRewardsTab.tsx
@@ -191,6 +191,7 @@ const CardGridContainer = styled(StakingCard)`
 `;
 
 const Value = styled(Body).attrs({ variant: 'bold', mono: true })`
+	color: ${(props) => props.theme.colors.selectedTheme.yellow};
 	font-size: 26px;
 	margin-top: 10px;
 `;
