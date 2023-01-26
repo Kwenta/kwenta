e524e0b97c8e066531884a266fdc73b2ea4b5455
pldespaigne <pl.despaigne@gmail.com>
2023-01-26 10:41:40 +0100

    add share button

--- sections/futures/MobileTrade/PositionDetails.tsx
+++ sections/futures/MobileTrade/PositionDetails.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 
+import UploadIcon from 'assets/svg/futures/upload-icon.svg';
 import { SectionHeader, SectionTitle } from 'sections/futures/mobile';
-import { selectPosition } from 'state/futures/selectors';
+import { selectMarketAsset, selectPosition } from 'state/futures/selectors';
 import { useAppSelector } from 'state/hooks';
+import { resetButtonCSS } from 'styles/common';
 
 import PositionCard from '../PositionCard';
 import EmptyPositionCard from '../PositionCard/EmptyPositionCard';
+import ShareModal from '../ShareModal';
 
 const PositionDetails = () => {
 	const position = useAppSelector(selectPosition);
+	const marketAsset = useAppSelector(selectMarketAsset);
+
+	const [showShareModal, setShowShareModal] = useState(false);
+
+	const handleOpenShareModal = useCallback(() => {
+		setShowShareModal(!showShareModal);
+	}, [showShareModal]);
 
 	return position?.position ? (
-		<PositionDetailsContainer>
-			<SectionHeader>
-				<SectionTitle>Open Position</SectionTitle>
-			</SectionHeader>
-			<PositionCard />
-		</PositionDetailsContainer>
+		<>
+			<PositionDetailsContainer>
+				<SectionHeader>
+					<SectionTitle>Open Position</SectionTitle>
+					<IconButton onClick={handleOpenShareModal}>
+						<UploadIcon />
+					</IconButton>
+				</SectionHeader>
+				<PositionCard />
+			</PositionDetailsContainer>
+			{showShareModal && (
+				<ShareModal
+					position={position}
+					marketAsset={marketAsset}
+					setShowShareModal={setShowShareModal}
+				/>
+			)}
+		</>
 	) : (
 		<PositionDetailsContainer>
 			<SectionHeader>
@@ -30,6 +52,18 @@ const PositionDetails = () => {
 
 const PositionDetailsContainer = styled.div`
 	margin: 15px;
+`;
+
+const IconButton = styled.button`
+	${resetButtonCSS};
+	padding-top: 5px;
+	padding-left: 64px;
+
+	svg {
+		width: 14px;
+		height: 14px;
+		fill: ${(props) => props.theme.colors.selectedTheme.gray};
+	}
 `;
 
 export default PositionDetails;
