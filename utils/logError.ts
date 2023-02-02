3268fb54c5518bcb29698566279c5a18a3f3ee1d
Adam Clarke <adam@adamclarke.co>
2023-02-02 14:40:11 +0000

    fix: check if approved/valid before est. exchange tx fee (#1946)
    * Check if approved before estimating exchange tx fee
    
    * Add Sentry ignore list

--- utils/logError.ts
+++ utils/logError.ts
@@ -3,5 +3,15 @@ import * as Sentry from '@sentry/browser';
 export default function logError(err: Error): void {
 	// eslint-disable-next-line no-console
 	console.error(err);
-	Sentry.captureException(err);
+	if (!sentrySkipFilter(err)) {
+		Sentry.captureException(err);
+	}
 }
+
+const sentrySkipFilter = (e: Error) => {
+	return IGNORE_ERRORS.some((text) => {
+		return e.message?.toLowerCase().includes(text.toLowerCase());
+	});
+};
+
+export const IGNORE_ERRORS = ['Insufficient margin', 'Network request failed', 'timeout exceeded'];
