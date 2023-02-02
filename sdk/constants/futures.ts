3385d335346fb96e04d5e5144c397b898ab075a8
Troy <troy.burmeister@gmail.com>
2023-02-02 11:36:39 -0700

    update mainnet subgraph endpoints

--- sdk/constants/futures.ts
+++ sdk/constants/futures.ts
@@ -1,7 +1,7 @@
 import { FuturesMarketAsset, FuturesMarketConfig, FuturesMarketKey } from 'sdk/types/futures';
 
 export const FUTURES_ENDPOINT_OP_MAINNET =
-	'https://api.thegraph.com/subgraphs/name/kwenta/optimism-futures';
+	'https://subgraph.satsuma-prod.com/05943208e921/kwenta/optimism-futures/api';
 
 export const FUTURES_ENDPOINT_OP_GOERLI =
 	'https://api.thegraph.com/subgraphs/name/kwenta/optimism-goerli-futures';
