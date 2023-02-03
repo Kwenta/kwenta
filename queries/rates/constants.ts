9a30e20a55a5f718af4546bb454e61bd54fdc9ae
Troy <troy.burmeister@gmail.com>
2023-02-03 12:43:15 -0700

    add satsuma key

--- queries/rates/constants.ts
+++ queries/rates/constants.ts
@@ -2,8 +2,7 @@ import { chain } from 'containers/Connector/config';
 
 export const RATES_ENDPOINT_MAIN = 'https://api.thegraph.com/subgraphs/name/kwenta/mainnet-main';
 
-export const RATES_ENDPOINT_OP_MAINNET =
-	'https://api.thegraph.com/subgraphs/name/kwenta/optimism-latest-rates';
+export const RATES_ENDPOINT_OP_MAINNET = `https://subgraph.satsuma-prod.com/${process.env.NEXT_PUBLIC_SATSUMA_API_KEY}/kwenta/optimism-latest-rates/api`;
 
 export const RATES_ENDPOINT_OP_GOERLI =
 	'https://api.thegraph.com/subgraphs/name/kwenta/optimism-goerli-latest-rates';
