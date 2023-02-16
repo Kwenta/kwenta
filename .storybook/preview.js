161e999eeaa1a68f6a9357b3840875d414ba751d
Oluwakorede Fashokun <koredefashokun@gmail.com>
2023-02-16 09:45:44 -0500

    Remove Redux from Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,8 +5,6 @@ import { themes } from '../styles/theme';
 import '../styles/main.css';
 import { getDesignTokens } from 'utils/theme';
 import * as NextImage from 'next/image';
-import { Provider as ReduxProvider } from 'react-redux';
-import { store } from 'state/store';
 
 const OriginalNextImage = NextImage.default;
 
