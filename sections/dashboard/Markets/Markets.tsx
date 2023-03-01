f3e2de9f99c975b6cc13b2fef97910458157f81d
Flocqst <flocastet@icloud.com>
2023-03-01 16:04:43 +0100

    remove futures markets button and clean unused code

--- sections/dashboard/Markets/Markets.tsx
+++ sections/dashboard/Markets/Markets.tsx
@@ -1,12 +1,8 @@
 import { FC, useState, useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
 
-import TabButton from 'components/Button/TabButton';
-import { DesktopOnlyView } from 'components/Media';
 import { TabPanel } from 'components/Tab';
 
 import FuturesMarketsTable from '../FuturesMarketsTable';
-import { TabButtonsContainer } from '../History/History';
 
 export enum MarketsTab {
 	FUTURES = 'futures',
@@ -14,34 +10,10 @@ export enum MarketsTab {
 }
 
 const Markets: FC = () => {
-	const { t } = useTranslation();
-
-	const [activeMarketsTab, setActiveMarketsTab] = useState<MarketsTab>(MarketsTab.FUTURES);
-
-	const MARKETS_TABS = useMemo(
-		() => [
-			{
-				name: MarketsTab.FUTURES,
-				label: t('dashboard.overview.markets-tabs.futures'),
-				active: activeMarketsTab === MarketsTab.FUTURES,
-				onClick: () => {
-					setActiveMarketsTab(MarketsTab.FUTURES);
-				},
-			},
-		],
-		[activeMarketsTab, t]
-	);
+	const [activeMarketsTab] = useState<MarketsTab>(MarketsTab.FUTURES);
 
 	return (
 		<>
-			<DesktopOnlyView>
-				<TabButtonsContainer>
-					{MARKETS_TABS.map(({ name, label, active, onClick }) => (
-						<TabButton key={name} title={label} active={active} onClick={onClick} />
-					))}
-				</TabButtonsContainer>
-			</DesktopOnlyView>
-
 			<TabPanel name={MarketsTab.FUTURES} activeTab={activeMarketsTab}>
 				<FuturesMarketsTable />
 			</TabPanel>
