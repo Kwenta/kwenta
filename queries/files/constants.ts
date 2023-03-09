04a92d8bbbf7a65a31c768f275292877267adfc7
Troy <troy.burmeister@gmail.com>
2023-03-09 14:19:33 -0700

    undo url

--- queries/files/constants.ts
+++ queries/files/constants.ts
@@ -1,3 +1,3 @@
 export const FLEEK_STORAGE_BUCKET = '25710180-23d8-43f4-b0c9-5b7f55f63165-bucket';
 
-export const FLEEK_BASE_URL = 'https://storage.fleek.zone';
+export const FLEEK_BASE_URL = 'https://storage.kwenta.io';
