44e013af41b4738afa9318dc053561a747e01d85
Leifu Chen <leifu.chen@gmail.com>
2023-03-09 19:53:56 +0200

    Added shard

--- components/ChatRoom/ChatRoom.tsx
+++ components/ChatRoom/ChatRoom.tsx
@@ -5,6 +5,7 @@ export default function ChatRoom() {
 		<Widgetbot
 			server="852273007370960937"
 			channel="852528616533000213"
+			shard="https://emerald.widgetbot.io"
 			width={'100%'}
 			height={'275px'}
 		/>
