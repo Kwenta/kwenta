cc69b395c6c7f4034d5441c0c7a52385e546b48e
avclarke <adam@adamclarke.co>
2023-03-18 20:37:46 +0000

    Use wagmi network switcher

--- hooks/useNetworkSwitcher.ts
+++ hooks/useNetworkSwitcher.ts
@@ -1,17 +1,18 @@
-import { addOptimismNetworkToMetamask } from '@synthetixio/optimism-networks';
 import { L2_TO_L1_NETWORK_MAPPER } from '@synthetixio/optimism-networks';
 import { utils, BigNumber } from 'ethers';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useConnect } from 'wagmi';
+import { useConnect, useSwitchNetwork } from 'wagmi';
 
 import Connector from 'containers/Connector';
 import { chain } from 'containers/Connector/config';
+import { hexlify } from 'ethers/lib/utils.js';
 
 const useNetworkSwitcher = () => {
 	const { network, isWalletConnected } = Connector.useContainer();
 	const { connect: connectWallet } = useConnect();
 	const [, setNetworkError] = useState<string | null>(null);
+	const { switchNetwork } = useSwitchNetwork();
 
 	const { t } = useTranslation();
 
@@ -38,11 +39,11 @@ const useNetworkSwitcher = () => {
 	const switchToL2 = async () => {
 		if (!isWalletConnected) connectWallet();
 		try {
-			if (!window.ethereum || !window.ethereum.isMetaMask) {
+			if (!switchNetwork) {
 				return setNetworkError(t('user-menu.error.please-install-metamask'));
 			}
 			setNetworkError(null);
-			addOptimismNetworkToMetamask({ ethereum: window.ethereum });
+			switchNetwork(10);
 		} catch (e) {
 			setNetworkError(e.message);
 		}
