bd229596a1097f508764344f7d789f7a9f2872f7
troyb.eth <me@troyb.xyz>
2023-03-31 14:34:55 -0600

    Smart margin: trade history (#2142)
    display trades

--- state/futures/actions.ts
+++ state/futures/actions.ts
@@ -829,6 +829,7 @@ export const fetchFuturesPositionHistory = createAsyncThunk<
 			accountType: FuturesAccountType;
 			history: FuturesPositionHistory<string>[];
 			account: string;
+			wallet: string;
 			networkId: NetworkId;
 	  }
 	| undefined,
@@ -843,7 +844,7 @@ export const fetchFuturesPositionHistory = createAsyncThunk<
 		const futuresSupported = selectFuturesSupportedNetwork(getState());
 		if (!wallet || !account || !futuresSupported) return;
 		const history = await sdk.futures.getPositionHistory(wallet);
-		return { accountType, account, networkId, history: serializePositionHistory(history) };
+		return { accountType, account, wallet, networkId, history: serializePositionHistory(history) };
 	} catch (err) {
 		notifyError('Failed to fetch position history', err);
 		throw err;
