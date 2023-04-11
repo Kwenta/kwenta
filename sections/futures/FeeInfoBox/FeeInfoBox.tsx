2d9b5a7eb2236283f9ed2be1bd5987120431d50f
platschi <platschi@posteo.org>
2023-04-11 11:20:52 -0300

    chore: implement kip-70 for 2wks

--- sections/futures/FeeInfoBox/FeeInfoBox.tsx
+++ sections/futures/FeeInfoBox/FeeInfoBox.tsx
@@ -172,19 +172,19 @@ const TradingRewardRow = memo(() => {
 
 	return (
 		<InfoBoxRow
-			title="Trading Reward"
+			title="Trading Rewards"
 			compactBox
 			value=""
 			keyNode={
 				<CompactBox $isEligible={isRewardEligible} onClick={goToStaking}>
 					<FlexDivRow style={{ marginBottom: '5px' }}>
 						<div>{t('dashboard.stake.tabs.trading-rewards.trading-reward')}</div>
-						<Badge color={isRewardEligible ? 'yellow' : 'red'}>
+						<Badge color={isRewardEligible ? 'yellow' : 'yellow'}>
 							{t(`dashboard.stake.tabs.trading-rewards.${isRewardEligible ? '' : 'not-'}eligible`)}
 							{isRewardEligible ? (
 								<EligibleIcon viewBox="0 0 8 8" style={{ paddingLeft: '2px' }} />
 							) : (
-								<NotEligibleIcon width="12" height="12" viewBox="-1.5 -0.5 9 9" />
+								<EligibleIcon viewBox="0 0 8 8" style={{ paddingLeft: '2px' }} />
 							)}
 						</Badge>
 					</FlexDivRow>
