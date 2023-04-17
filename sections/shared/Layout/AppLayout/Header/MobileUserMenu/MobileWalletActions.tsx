18fe5583d39a8f5d1ab5825c33193cd47afd5a9d
Oluwakorede Fashokun <koredefashokun@gmail.com>
2023-04-17 10:27:04 -0400

    Toggle account modal

--- sections/shared/Layout/AppLayout/Header/MobileUserMenu/MobileWalletActions.tsx
+++ sections/shared/Layout/AppLayout/Header/MobileUserMenu/MobileWalletActions.tsx
@@ -1,3 +1,4 @@
+import { useAccountModal } from '@rainbow-me/rainbowkit';
 import { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useEnsAvatar, useEnsName } from 'wagmi';
@@ -12,19 +13,20 @@ type MobileWalletButtonProps = {
 	toggleModal(): void;
 };
 
-export const MobileWalletActions: FC<MobileWalletButtonProps> = ({ toggleModal }) => {
+export const MobileWalletActions: FC<MobileWalletButtonProps> = () => {
 	const { walletAddress } = Connector.useContainer();
 	const { data: ensAvatar } = useEnsAvatar({ address: walletAddress!, chainId: 1 });
 	const { data: ensName } = useEnsName({ address: walletAddress!, chainId: 1 });
 	const [walletLabel, setWalletLabel] = useState<string>('');
 	const truncatedWalletAddress = truncateAddress(walletAddress! ?? '');
+	const { openAccountModal } = useAccountModal();
 
 	useEffect(() => {
 		setWalletLabel(ensName || truncatedWalletAddress!);
 	}, [ensName, truncatedWalletAddress]);
 
 	return (
-		<StyledButton mono noOutline onClick={toggleModal}>
+		<StyledButton mono noOutline onClick={openAccountModal}>
 			{ensAvatar ? (
 				<StyledImage src={ensAvatar} alt={ensName || walletAddress!} width={16} height={16} />
 			) : (
