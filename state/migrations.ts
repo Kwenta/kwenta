4ab677db6d4045b5f4f30ae04359495f2545cee5
leifu <leifu.chen@gmail.com>
2023-04-19 22:56:45 +0300

    chore: add home to migration (#2246)
    * Try to add home state into migration
    
    * Bump the migration to 22

--- state/migrations.ts
+++ state/migrations.ts
@@ -52,15 +52,10 @@ export const migrations = {
 			futures: FUTURES_INITIAL_STATE,
 		};
 	},
-	9: (state: any) => {
+	22: (state: any) => {
 		return {
 			...state,
 			home: HOME_INITIAL_STATE,
-		};
-	},
-	21: (state: any) => {
-		return {
-			...state,
 			futures: FUTURES_INITIAL_STATE,
 			earn: EARN_INITIAL_STATE,
 			staking: STAKING_INITIAL_STATE,
