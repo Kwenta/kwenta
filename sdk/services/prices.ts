548a38aaa1374af988ccffa76cb5573f2aa50477
Leifu Chen <leifu.chen@gmail.com>
2023-05-24 01:25:00 +0300

    Clean the code

--- sdk/services/prices.ts
+++ sdk/services/prices.ts
@@ -188,13 +188,11 @@ export default class PricesService {
 		const prices = (response ? Object.values(response).flat() : []) as SynthPrice[];
 
 		//TODO: remove this once the rates are fixed
-		const newPrices = prices.map((price) =>
+		return prices.map((price) =>
 			price.synth === MarketAssetByKey[FuturesMarketKey.sPEPEPERP]
 				? { ...price, rate: wei(price.rate.toString()).div(1e10).toString() }
 				: price
-		);
-
-		return newPrices;
+		) as SynthPrice[];
 	}
 
 	public async getPythPriceUpdateData(marketKey: FuturesMarketKey) {
