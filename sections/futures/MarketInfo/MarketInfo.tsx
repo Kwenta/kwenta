27c698ddfd7cd664cc640694f1d47b20425a962d
leifu <leifu.chen@gmail.com>
2023-05-30 17:10:43 +0300

    fix: banner style and TV chart (#2406)
    * Fixed the height of tv chart
    
    * Update the color

--- sections/futures/MarketInfo/MarketInfo.tsx
+++ sections/futures/MarketInfo/MarketInfo.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import media from 'styles/media';
+import { selectShowBanner } from 'state/app/selectors';
+import { useAppSelector } from 'state/hooks';
 
 import UserInfo from '../UserInfo';
 import ChartWrapper from './ChartWrapper';
 
 const MarketInfo: React.FC = React.memo(() => {
+	const showBanner = useAppSelector(selectShowBanner);
+
 	return (
-		<Container>
+		<Container showBanner={showBanner}>
 			<ChartWrapper />
 			<UserInfo />
 		</Container>
 	);
 });
 
-const Container = styled.div`
+const Container = styled.div<{ showBanner?: boolean }>`
 	height: 100%;
 	width: 100%;
 	overflow: hidden;
 	display: grid;
-	grid-template-rows: 1fr 320px;
-	${media.lessThan('xl')`
-		grid-template-rows: 1fr 240px;
-	`}
+	grid-template-rows: 1fr calc(320px - ${(props) => (props.showBanner ? 50 : 0)}px);
 	border-left: ${(props) => props.theme.colors.selectedTheme.border};
 `;
 
