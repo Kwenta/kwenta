cd87951286a125557e9c1c070c76b4fa601d7f30
leifu <leifu.chen@gmail.com>
2023-06-04 16:34:10 +0300

    chore: add close only prompt (#2421)
    * Add a close only prompt
    
    * Add prompt to mobile
    
    * Clean the code
    
    * Reorganized the code
    
    * Cleaned the code
    
    * Removed the unnecessary changes

--- sections/futures/Trade/TradePanel.tsx
+++ sections/futures/Trade/TradePanel.tsx
@@ -16,6 +16,7 @@ import LeverageInput from '../LeverageInput';
 import MarginInput from '../MarginInput';
 import OrderSizing from '../OrderSizing';
 import PositionButtons from '../PositionButtons';
+import CloseOnlyPrompt from './CloseOnlyPrompt';
 import ManagePosition from './ManagePosition';
 import OrderAcknowledgement from './OrderAcknowledgement';
 import OrderTypeSelector from './OrderTypeSelector';
@@ -54,7 +55,9 @@ const TradePanel: FC<Props> = memo(({ mobile }) => {
 		}
 	}, [orderType, hideOrderWarning]);
 
-	return (
+	return process.env.NEXT_PUBLIC_CLOSE_ONLY === 'true' ? (
+		<CloseOnlyPrompt $mobile={mobile} />
+	) : (
 		<TradePanelContainer $mobile={mobile}>
 			{!mobile && <TradeBalance />}
 			<PositionButtons selected={leverageSide} onSelect={handleChangeSide} />
