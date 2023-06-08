141cf3e5686a6344958b09f57d302d61f996a6fa
Oluwakorede Fashokun <koredefashokun@gmail.com>
2023-06-08 10:52:29 -0400

    Fix issue with fullscreen search

--- components/Media/Media.tsx
+++ components/Media/Media.tsx
@@ -11,29 +11,24 @@ const MediaQuery = dynamic(() => import('react-responsive'), {
 	ssr: false,
 });
 
-// <Media greaterThanOrEqual="md">{children}</Media>
 export const DesktopOnlyView: FC<MediaProps> = memo(({ children }) => (
 	<MediaQuery minWidth={BREAKPOINTS.md}>{children}</MediaQuery>
 ));
 
-// <Media between={['sm', 'md']}>{children}</Media>
 export const TabletOnlyView: FC<MediaProps> = memo(({ children }) => (
 	<MediaQuery minWidth={BREAKPOINTS.sm} maxWidth={BREAKPOINTS.md - 1}>
 		{children}
 	</MediaQuery>
 ));
 
-// <Media lessThan="md">{children}</Media>
 export const MobileOrTabletView: FC<MediaProps> = memo(({ children }) => (
 	<MediaQuery maxWidth={BREAKPOINTS.md - 1}>{children}</MediaQuery>
 ));
 
-// <Media greaterThan="xs">{children}</Media>
 export const MobileHiddenView: FC<MediaProps> = memo(({ children }) => (
 	<MediaQuery minWidth={BREAKPOINTS.sm}>{children}</MediaQuery>
 ));
 
-// <Media at="xs">{children}</Media>
 export const MobileOnlyView: FC<MediaProps> = memo(({ children }) => (
 	<MediaQuery minWidth={BREAKPOINTS.xs} maxWidth={BREAKPOINTS.sm - 1}>
 		{children}
