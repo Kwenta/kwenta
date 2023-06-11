dbe79888be8f25e0285cdfb0178585da6ea91114
Leifu Chen <leifu.chen@gmail.com>
2023-06-12 02:33:21 +0300

    Adjusted the step to 0.1%

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -45,7 +45,7 @@ export const getStep = (maxValue: number) => {
 	if (maxValue < 10) return 0.01;
 	if (maxValue < 100) return 1;
 	if (maxValue < 10000) return 10;
-	return Math.pow(10, Math.floor(Math.log10(maxValue)) - 1);
+	return Math.pow(10, Math.floor(Math.log10(maxValue)) - 3);
 };
 
 const styledMarkLabel = css`
