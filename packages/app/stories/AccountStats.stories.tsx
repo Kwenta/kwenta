f016317c7b4c8ffb13e77f7a8399a87dac244d49
Oluwakorede Fashokun <koredefashokun@gmail.com>
2023-06-22 04:18:37 -0400

    Re-add stories

--- packages/app/stories/AccountStats.stories.tsx
+++ packages/app/stories/AccountStats.stories.tsx
@@ -1,7 +1,18 @@
+import { InfoBoxContainer, InfoBoxRow } from 'components/InfoBox'
+
 export default {
 	title: 'Futures/AccountStats',
+	component: <div />,
 }
 
 export const Default = () => {
-	return <div style={{ width: 334 }}></div>
+	return (
+		<div style={{ width: 334 }}>
+			<InfoBoxContainer>
+				<InfoBoxRow title="Available Margin" value="$27,192.98" />
+				<InfoBoxRow title="Buying Power" value="$271,929.80" />
+				<InfoBoxRow title="Margin Usage" value="10%" />
+			</InfoBoxContainer>
+		</div>
+	)
 }
