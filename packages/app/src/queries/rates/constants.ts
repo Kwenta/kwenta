6f671d8b43cd3818911b019bd73fb265c8656a97
Leifu Chen <leifu.chen@gmail.com>
2023-07-15 14:43:18 +0300

    Update the pyth trading view endpoint

--- packages/app/src/queries/rates/constants.ts
+++ packages/app/src/queries/rates/constants.ts
@@ -3,4 +3,5 @@ export const COMMODITIES_BASE_API_URL =
 
 export const FOREX_BASE_API_URL = 'https://api.exchangerate.host/latest'
 
-export const DEFAULT_PYTH_TV_ENDPOINT = 'https://pyth-api.vintage-orange-muffin.com/v2/history'
+export const DEFAULT_PYTH_TV_ENDPOINT =
+	'https://benchmarks.pyth.network/v1/shims/tradingview/history'
