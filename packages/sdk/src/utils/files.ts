350ae006fdb8c3dbb92c393042d3d8ad386a3eb4
leifu <leifu.chen@gmail.com>
2023-07-19 11:31:41 +0300

    Increased the timeout to 10s

--- packages/sdk/src/utils/files.ts
+++ packages/sdk/src/utils/files.ts
@@ -4,5 +4,5 @@ import { FLEEK_BASE_URL, FLEEK_STORAGE_BUCKET } from '../constants/files'
 
 export const client = axios.create({
 	baseURL: `${FLEEK_BASE_URL}/${FLEEK_STORAGE_BUCKET}/data/`,
-	timeout: 5000,
+	timeout: 10000,
 })
