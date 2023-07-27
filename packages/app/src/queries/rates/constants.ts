4b08cc6c035a81908a00617d9d1efc6a8aa0479d
leifu <leifu.chen@gmail.com>
2023-07-27 15:47:26 +0300

    fix: forex prefix (#2687)
    * Added forex assets
    
    * Cleaned the code

--- packages/app/src/queries/rates/constants.ts
+++ packages/app/src/queries/rates/constants.ts
@@ -1,3 +1,5 @@
+import { AssetTypes } from './types'
+
 export const COMMODITIES_BASE_API_URL =
 	'https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument'
 
@@ -5,3 +7,8 @@ export const FOREX_BASE_API_URL = 'https://api.exchangerate.host/latest'
 
 export const DEFAULT_PYTH_TV_ENDPOINT =
 	'https://benchmarks.pyth.network/v1/shims/tradingview/history'
+
+export const NON_CRYPTO_ASSET_TYPES: AssetTypes = {
+	Metal: ['XAU', 'XAG'],
+	FX: ['EUR', 'GBP', 'AUD'],
+}
