63bdde423c981ee7ba8985be02007ce69a33b427
Adam Clarke <adam@adamclarke.co>
2023-09-14 23:21:44 +0100

    chore(*): fix chart loading times after navigating from home page (#971)
    * Use proxy for pyth price history
    
    * Home page - reduce markets polling and reduce num of market cards shown
    
    * chore: revert DEFAULT_PYTH_TV_ENDPOINT
    
    ---------
    
    Co-authored-by: platschi <platschi@posteo.org>

--- packages/app/src/queries/rates/constants.ts
+++ packages/app/src/queries/rates/constants.ts
@@ -5,7 +5,7 @@ export const COMMODITIES_BASE_API_URL =
 
 export const FOREX_BASE_API_URL = 'https://api.exchangerate.host/latest'
 
-export const DEFAULT_PYTH_TV_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVICES_PROXY}/price-history/v1/shims/tradingview/history`
+export const DEFAULT_PYTH_TV_ENDPOINT = `https://benchmarks.pyth.network/v1/shims/tradingview/history`
 
 export const NON_CRYPTO_ASSET_TYPES: AssetTypes = {
 	Metal: ['XAU', 'XAG'],
